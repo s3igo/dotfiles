b4451f2a19a510bc31ffbb81811e1cbd3bb82cc1
s3igo <85787242+SeigoMori@users.noreply.github.com>
2022-05-29 19:52:44 +0900

    add: 現在のshellに応じて.OOshrcを削除

--- bin/link.sh
+++ bin/link.sh
@@ -2,10 +2,23 @@
 
 DOT_DIR=~/.dotfiles
 
-[ "$SHELL" = '/bin/zsh' ] && ln -fnsv "$DOT_DIR/shell/.zshrc" "$HOME/.zshrc"
-[ "$SHELL" = '/bin/bash' ] && ln -fnsv "$DOT_DIR/shell/.bashrc" "$HOME/.bashrc"
-[ "$SHELL" = '/bin/bash' ] && ln -fnsv "$DOT_DIR/shell/.bash_profile" "$HOME/.bash_profile"
+# shell
+[ "$SHELL" = '/bin/zsh' ] \
+	&& ln -fnsv "$DOT_DIR/shell/.zshrc" "$HOME/.zshrc" \
+	&& [ -f "$HOME/.bashrc" -a -f "$HOME/.bash_profile" ] \
+	&& rm -f "$HOME/.bashrc" "$HOME/.bash_profile"
 
+[ "$SHELL" = '/bin/bash' ] \
+	&& ln -fnsv "$DOT_DIR/shell/.bashrc" "$HOME/.bashrc" \
+	&& ln -fnsv "$DOT_DIR/shell/.bash_profile" "$HOME/.bash_profile" \
+	&& [ -f "$HOME/.zshrc" ] \
+	&& rm -f "$HOME/.zshrc"
+
+# vim
 ln -fnsv "$DOT_DIR/vim/.vimrc" "$HOME/.vimrc"
+
+# vscode-settings
 ln -fnsv "$DOT_DIR/others/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+
+# global-gitignore
 ln -fnsv "$DOT_DIR/others/.gitignore_global" "$HOME/.config/git/ignore"
