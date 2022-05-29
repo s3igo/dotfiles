ca088c2bd97c0cedb21a108c1d30108047bc4cdd
s3igo <85787242+SeigoMori@users.noreply.github.com>
2022-05-29 18:19:28 +0900

    change: 未完成 or 環境依存の設定ファイルを削除

--- bin/link.sh
+++ bin/link.sh
@@ -2,13 +2,10 @@
 
 DOT_DIR=~/.dotfiles
 
-ln -fnsv "$DOT_DIR/others/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
-ln -fnsv "$DOT_DIR/others/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
-ln -fnsv "$DOT_DIR/others/ignore" "$HOME/.config/git/ignore"
-ln -fnsv "$DOT_DIR/others/init.el" "$HOME/.emacs.d/init.el"
-ln -fnsv "$DOT_DIR/others/init.vim" "$HOME/.config/nvim/init.vim"
 [ "$SHELL" = '/bin/zsh' ] && ln -fnsv "$DOT_DIR/shell/.zshrc" "$HOME/.zshrc"
 [ "$SHELL" = '/bin/bash' ] && ln -fnsv "$DOT_DIR/shell/.bashrc" "$HOME/.bashrc"
 [ "$SHELL" = '/bin/bash' ] && ln -fnsv "$DOT_DIR/shell/.bash_profile" "$HOME/.bash_profile"
+
 ln -fnsv "$DOT_DIR/vim/.vimrc" "$HOME/.vimrc"
-# ln -fnsv "$DOT_DIR/.karabiner.json" "$HOME/.config/karabiner/assets/complex_modifications/myKarabiner.json"
+ln -fnsv "$DOT_DIR/others/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+ln -fnsv "$DOT_DIR/others/ignore" "$HOME/.config/git/ignore"
