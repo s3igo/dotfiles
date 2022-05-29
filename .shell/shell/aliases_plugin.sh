1b7882474663d8d9fb0b4574afe3a404576ab6fe
s3igo <85787242+SeigoMori@users.noreply.github.com>
2022-05-29 16:19:32 +0900

    change: pluginとzshのaliasファイルを統合

--- .shell/shell/aliases_plugin.sh
+++
@@ -1,9 +0,0 @@
-# anyframe
-alias cd/='l
-	| anyframe-selector-auto \
-	| anyframe-action-execute cd --'
-alias cd.='ls -a \
-	| tail -n +3 \
-	| anyframe-selector-auto \
-	| anyframe-action-execute cd --'
-alias pk=anyframe-widget-kill
--- .shell/shell/aliases_z.sh
+++ .shell/shell/aliases_z.sh
@@ -15,9 +15,17 @@ alias -g Pa='`docker ps -a --format "table {{.ID}} {{.Names}}\t{{.Image}}\t{{.St
 	| anyframe-selector-auto \
 	| cut -d " " -f 1`'
 
+# anyframe
+alias cc='anyframe-widget-cdr && code .'
+alias cd/='l
+	| anyframe-selector-auto \
+	| anyframe-action-execute cd --'
+alias cd.='ls -a \
+	| tail -n +3 \
+	| anyframe-selector-auto \
+	| anyframe-action-execute cd --'
+alias cdr=anyframe-widget-cdr
+alias pk=anyframe-widget-kill
+
 # shell
 alias sz='source ~/.zshrc'
-
-# anyframe
-alias cdr=anyframe-widget-cdr
-alias cc='anyframe-widget-cdr && code .'
