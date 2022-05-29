a91cec9a449dc12abcab7514d69566fdc6f8f66c
s3igo <85787242+SeigoMori@users.noreply.github.com>
2022-05-30 02:42:17 +0900

    change: zsh plugin settings config.sh to zinit.sh
    条件分岐減らすため

--- shell/lib/zinit.sh
+++ shell/lib/zinit.sh
@@ -20,3 +20,53 @@ zdharma-continuum/zinit-annex-patch-dl \
 zdharma-continuum/zinit-annex-rust
 
 ### End of Zinit's installer chunk
+
+
+# plugin
+## theme
+zinit ice pick"async.zsh" src"pure.zsh"
+zinit light sindresorhus/pure
+
+## autosuggestion
+zinit ice wait'!0'
+zinit light zsh-users/zsh-autosuggestions
+
+## tab-completion
+zinit ice wait'!0'
+zinit light zsh-users/zsh-completions
+
+## syntax-highlight
+zinit ice wait'!0'
+zinit light zdharma-continuum/fast-syntax-highlighting
+
+## open on GitHub
+zinit ice wait'!0'
+zinit light paulirish/git-open
+
+## auto input closure/quote
+zinit ice wait'!0'
+zinit light momo-lab/zsh-smartinput
+
+## remove doller from the beginning of line
+zinit ice wait'!0'
+zinit light zpm-zsh/undollar
+
+## anyframe
+zinit ice wait'!0'
+zinit light mollifier/anyframe
+
+### cdr
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+zle -N anyframe-widget-cdr
+
+### command history
+zle -N anyframe-widget-execute-history
+bindkey '^R' anyframe-widget-execute-history
+
+### process kill
+zle -N anyframe-widget-kill
+
+### ghq
+zle -N anyframe-widget-cd-ghq-repository
+bindkey '^G' anyframe-widget-cd-ghq-repository
