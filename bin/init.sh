82913a6bd0ecfe26b5b0e97f9a2540fb07278877
s3igo <85787242+SeigoMori@users.noreply.github.com>
2022-05-29 16:20:19 +0900

    change: init.sh -> install.shに名前変更

--- bin/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$uname" = 'Darwin' ] ; then
-	xcode-select --install > /dev/null
-	/bin/zsh -c "$curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh" > /dev/null
-else
-	echo 'not MacOS'
-fi
---
+++ bin/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install Xcode-CLI
+[ "$uname" = 'Darwin' ] && which xcode-select > /dev/null 2>&1 || xcode-select --install
+
+# install brew
+which brew > /dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# brew update
+which brew > /dev/null 2>&1 && brew update
