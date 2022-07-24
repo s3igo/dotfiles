dab2e067ef6f48ae2b05db53b53af2d3a8b2fd0b
s3igo <85787242+SeigoMori@users.noreply.github.com>
2022-07-24 19:44:05 +0900

    remove: env.sh

--- shell/lib/env.sh
+++
@@ -1,6 +0,0 @@
-# php
-# export PATH="$PATH:usr/local/opt/bzip2/bin"
-# export PATH="$PATH:usr/local/opt/libiconv/bin"
-
-# TeX
-export PATH="$PATH:/usr/local/texlive/2022/bin/universal-darwin/tlmgr"
