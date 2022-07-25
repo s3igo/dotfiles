3508e6baca3bd66fc119e900f31358deb7096a2e
s3igo <85787242+s3igo@users.noreply.github.com>
2022-07-25 21:56:22 +0900

    fix: AppleSiliconの場合のhomebrewの導入に対応

--- install.sh
+++ install.sh
@@ -9,7 +9,10 @@
 	|| xcode-select --install
 
 # install brew
-which brew > /dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which brew > /dev/null 2>&1 \
+	|| /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" \
+	&& [ "$(uname -m)" = 'arm64' ] \
+	&& "$(/opt/homebrew/bin/brew shellenv)"
 
 # brew update
 which brew > /dev/null 2>&1 && brew update
