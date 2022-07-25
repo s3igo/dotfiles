f8eca0a5765b7a4794f6bbed1439df8cde817fae
s3igo <85787242+s3igo@users.noreply.github.com>
2022-07-25 20:46:50 +0900

    change: anyenv -> nodenv

--- shell/lib/config.sh
+++ shell/lib/config.sh
@@ -6,9 +6,15 @@ export LANG=ja_JP.UTF-8
 HISTSIZE=100000
 SAVEHIST=1000000
 
+# homebrew
+[ "uname -m" = 'arm64' ] && "$(/opt/homebrew/bin/brew shellenv)"
+
 # language
+export PATH="$PATH:$HOME/.nodenv/bin"
+eval "$(nodenv init -)"
+
 ## anyenv
-which anyenv > /dev/null 2>&1 && eval "$(anyenv init -)"
+# which anyenv > /dev/null 2>&1 && eval "$(anyenv init -)"
 
 ## ghcup-env
-which ghcup > /dev/null 2>&1 && [ -f "$HOME/.ghcup/env" ] && source "$HOME/.ghcup/env"
+# which ghcup > /dev/null 2>&1 && [ -f "$HOME/.ghcup/env" ] && source "$HOME/.ghcup/env"
