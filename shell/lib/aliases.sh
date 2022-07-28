948d3ee9d9339e7b2615db4cdfc23dbacf60621f
s3igo <85787242+s3igo@users.noreply.github.com>
2022-07-28 12:38:40 +0900

    clean: format

--- shell/lib/aliases.sh
+++ shell/lib/aliases.sh
@@ -18,18 +18,23 @@ alias gi="git init && git commit --allow-empty -m 'initial commit'"
 alias gp='git push origin main'
 
 # shell
+## to avoid making mistakes
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+## cd
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
-alias cp='cp -i'
-alias f='find'
-alias fn='find . -name'
+## ls
 alias la='ls -hla --color=auto'
 alias ll='ls -hl --color=auto'
 alias ls='ls --color=auto'
+## find
+alias f='find'
+alias fn='find . -name'
+## others
 alias mk='mkdir'
-alias mv='mv -i'
-alias relogin='exec $SHELL -l'
-alias rm='rm -i'
 alias to='touch'
 alias wh='which'
+alias relogin='exec $SHELL -l'
