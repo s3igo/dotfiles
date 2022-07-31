6b64a69032d557f2f2adf22284b1b8efcd408a36
s3igo <85787242+s3igo@users.noreply.github.com>
2022-07-31 19:34:24 +0900

    fix: armmacの場合のbrewのpath通す処理

--- shell/lib/config.sh
+++ shell/lib/config.sh
@@ -7,6 +7,4 @@ HISTSIZE=100000
 SAVEHIST=1000000
 
 # homebrew
-[ "$(uname -m)" = 'arm64' ] \
-	&& which brew > /dev/null 2>&1 \
-	&& eval "$(/opt/homebrew/bin/brew shellenv)"
+[ "$(uname -m)" = 'arm64' ] && eval "$(/opt/homebrew/bin/brew shellenv)"
