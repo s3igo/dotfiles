0ff5b35425eac63a0815c5d43a697dc234f7e4b1
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-02 13:55:29 +0900

    add: armmacの時のみ実行する

--- bin/install.sh
+++ bin/install.sh
@@ -4,7 +4,7 @@
 bash -c "$(curl -L raw.githubusercontent.com/s3igo/dotfiles/main/bin/init.sh)"
 
 # brew path
-eval "$(/opt/homebrew/bin/brew shellenv)"
+[ "$(uname -m)" = 'arm64' ] && eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # clone dotfiles
 git clone https://github.com/s3igo/dotfiles.git ~/.dotfiles
