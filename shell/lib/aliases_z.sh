67168502f3245fe383ac20ac437627a03f109ff8
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-05 12:28:47 +0900

    change: prefixを変更

--- shell/lib/aliases_z.sh
+++ shell/lib/aliases_z.sh
@@ -6,18 +6,18 @@ alias _cdr=anyframe-widget-cdr
 
 # global
 ## shell
-alias -g i='install'
-alias -g D='`ls | anyframe-selector-auto`'
-alias -g Da='`ls -a \
-	| tail -n +3 \
-	| anyframe-selector-auto`'
+alias -g _i='install'
+alias -g _ls='`ls | anyframe-selector-auto`'
+alias -g _ls-a='`ls -a \
+    | tail -n +3 \
+    | anyframe-selector-auto`'
 
 ## docker
-alias -g P='`docker ps --format "table {{.ID}} {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
-	| tail -n +2 \
-	| anyframe-selector-auto \
-	| cut -d " " -f 1`'
-alias -g Pa='`docker ps -a --format "table {{.ID}} {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
-	| tail -n +2 \
-	| anyframe-selector-auto \
-	| cut -d " " -f 1`'
+alias -g _dp='`docker ps --format "table {{.ID}} {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
+    | tail -n +2 \
+    | anyframe-selector-auto \
+    | cut -d " " -f 1`'
+alias -g _dp-a='`docker ps -a --format "table {{.ID}} {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
+    | tail -n +2 \
+    | anyframe-selector-auto \
+    | cut -d " " -f 1`'
