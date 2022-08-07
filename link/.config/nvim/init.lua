eff8cedd8d51c5d5d5a512e99a532995d99a75dc
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-08 01:40:31 +0900

    wip

--- link/.config/nvim/init.lua
+++ link/.config/nvim/init.lua
@@ -5,12 +5,19 @@ local l = vim.opt_local
 local g = vim.opt_global
 
 -- config
--- o.langmenu = 'en_US'
+vim.env.LANG = 'en_US.UTF-8'
+o.encoding = 'utf-8'
+o.fileencoding = 'utf-8'
 
 -- appearence
 o.number = true
-o.ambiwidth = 'single'
--- o.list = true
+o.ambiwidth = 'double'
+o.emoji = true
+o.cmdheight = 1
+o.ruler = true
+o.fileformats = { unix, dos, mac }
+o.list = true
+o.listchars = { space = '･', tab = '>-', trail = '*', eol = '¬', extends = '»' }
 
 -- tab
 o.tabstop = 4
@@ -20,5 +27,10 @@ o.expandtab = true
 o.autoindent = true
 o.smarttab = true
 
+-- keybind
+vim.g.mapleader = " "
+
+-- case
+o.ignorecase = true
 
 vim.cmd[[autocmd BufWritePost plugins.lua PackerCompile]]
