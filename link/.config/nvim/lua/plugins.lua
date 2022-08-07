849d84160efae660407704ce5ed3e102995355ad
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-07 23:49:40 +0900

    add: neo-tree

--- link/.config/nvim/lua/plugins.lua
+++ link/.config/nvim/lua/plugins.lua
@@ -1,8 +1,21 @@
-vim.cmd[[packadd packer.nvim]]
+vim.cmd [[packadd packer.nvim]]
 
 require('packer').startup(function(use)
     use("wbthomason/packer.nvim")
-    use("nvim-neo-tree/neo-tree.nvim")
     use("nvim-telescope/telescope.nvim")
+
+    -- filer
+    -- Unless you are still migrating, remove the deprecated commands from v1.x
+    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+    use {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        }
+    }
 
 end)
