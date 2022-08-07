b019a6c44689b4abfef8aefeec55c11fb2876116
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-07 14:11:30 +0900

    wip: ディレクトリ構成を変更、bash対応、./bin以下を修正

--- bin/devcontainer.sh
+++ bin/devcontainer.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo "$SHELL" | grep 'bash' > /dev/null 2>&1 \
-	&& ln -fnsv ~/.dotfiles/shell/.bashrc ~/.bashrc \
-	&& ln -fnsv ~/.dotfiles/shell/.bash_profile ~/.bash_profile
+	&& ln -fnsv ~/.dotfiles/link/.bashrc ~/.bashrc \
+	&& ln -fnsv ~/.dotfiles/link/.bash_profile ~/.bash_profile
 
-echo "$SHELL" | grep 'zsh' > /dev/null 2>&1 && ln -fnsv ~/.dotfiles/shell/.zshrc ~/.zshrc
-
-ln -fnsv ~/.dotfiles/vim/.vimrc ~/.vimrc
+ln -fnsv ~/.dotfiles/link/.vimrc ~/.vimrc
--- bin/link.sh
+++ bin/link.sh
@@ -1,46 +1,46 @@
 #!/bin/sh
 
 DOT_DIR=~/.dotfiles
+BASE_DIR=~/.dotfiles/link
 
 # shell
 echo "$SHELL" | grep 'zsh' > /dev/null 2>&1 \
-	&& ln -fnsv "$DOT_DIR/shell/.zshenv" "$HOME/.zshenv" \
+	&& ln -fnsv "$BASE_DIR/.zshenv" "$HOME/.zshenv" \
 	&& mkdir -p "$HOME/.config/zsh" \
-	&& ln -fnsv "$DOT_DIR/shell/.zshrc" "$HOME/.config/zsh/.zshrc" \
-	&& [ -f "$HOME/.bashrc" -a -f "$HOME/.bash_profile" ] \
-	&& rm -f "$HOME/.bashrc" "$HOME/.bash_profile"
+	&& ln -fnsv "$BASE_DIR/.config/zsh/.zshenv" "$HOME/.config/zsh/.zshenv" \
+	&& ln -fnsv "$BASE_DIR/.config/zsh/.zshrc" "$HOME/.config/zsh/.zshrc" \
 
 echo "$SHELL" | grep 'bash' > /dev/null 2>&1 \
-	&& ln -fnsv "$DOT_DIR/shell/.bashrc" "$HOME/.bashrc" \
-	&& ln -fnsv "$DOT_DIR/shell/.bash_profile" "$HOME/.bash_profile"
+	&& ln -fnsv "$BASE_DIR/.bashrc" "$HOME/.bashrc" \
+	&& ln -fnsv "$BASE_DIR/.bash_profile" "$HOME/.bash_profile"
 
 # vim
-ln -fnsv "$DOT_DIR/vim/.vimrc" "$HOME/.vimrc"
+ln -fnsv "$BASE_DIR/.vimrc" "$HOME/.vimrc"
 
 # vscode
 [ "$(uname)" = 'Darwin' ] \
 	&& mkdir -p "$HOME/Library/Application Support/Code/User" \
-	&& ln -fnsv "$DOT_DIR/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json" \
-	&& ln -fnsv "$DOT_DIR/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+	&& ln -fnsv "$BASE_DIR/_vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json" \
+	&& ln -fnsv "$BASE_DIR/_vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 
 [ "$(uname)" = 'Linux' ] \
 	&& mkdir -p "$HOME/.config/Code/User" \
-	&& ln -fnsv "$DOT_DIR/vscode/keybindings.json" "$HOME/.config/Code/User/settings.json" \
-	&& ln -fnsv "$DOT_DIR/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
+	&& ln -fnsv "$BASE_DIR/_vscode/keybindings.json" "$HOME/.config/Code/User/settings.json" \
+	&& ln -fnsv "$BASE_DIR/_vscode/settings.json" "$HOME/.config/Code/User/settings.json"
 
 # asdf
-ln -fnsv "$DOT_DIR/others/.tool-versions" "$HOME/.tool-versions"
+ln -fnsv "$BASE_DIR/.tool-versions" "$HOME/.tool-versions"
 
 # alacritty
 mkdir -p ~/.config/alacritty
-ln -fnsv "$DOT_DIR/others/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
+ln -fnsv "$BASE_DIR/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
 
 # global-gitignore
 mkdir -p ~/.config/git
-ln -fnsv "$DOT_DIR/others/.gitignore_global" "$HOME/.config/git/ignore"
+ln -fnsv "$BASE_DIR/.config/git/ignore" "$HOME/.config/git/ignore"
 
 # gitconfig
-ln -fnsv "$DOT_DIR/others/.gitconfig" "$HOME/.gitconfig"
+ln -fnsv "$BASE_DIR/.gitconfig" "$HOME/.gitconfig"
 
 # relogin shell
 exec $SHELL -l
