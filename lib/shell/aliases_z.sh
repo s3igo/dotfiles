c7c25735567a8f60d013287f7f35e6e184a93780
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-07 22:52:48 +0900

    change: format 補完がきれいに表示されるように

--- lib/shell/aliases_z.sh
+++ lib/shell/aliases_z.sh
@@ -5,16 +5,10 @@ alias _start='source ~/.config/zsh/.zshrc'
 ## shell
 alias -g _i='install'
 alias -g _ls='`ls | anyframe-selector-auto`'
-alias -g _ls-a='`ls -a \
-    | tail -n +3 \
-    | anyframe-selector-auto`'
+alias -g _ls-a='`ls -a | tail -n +3 | anyframe-selector-auto`'
 
 ## docker
 alias -g _dp='`docker ps --format "table {{.ID}} {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
-    | tail -n +2 \
-    | anyframe-selector-auto \
-    | cut -d " " -f 1`'
+    | tail -n +2 | anyframe-selector-auto | cut -d " " -f 1`'
 alias -g _dp-a='`docker ps -a --format "table {{.ID}} {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
-    | tail -n +2 \
-    | anyframe-selector-auto \
-    | cut -d " " -f 1`'
+    | tail -n +2 | anyframe-selector-auto | cut -d " " -f 1`'
