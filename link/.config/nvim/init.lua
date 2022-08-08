416dec30f82d466e7c3eda2029543382765513c2
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-08 10:28:51 +0900

    chore: format

--- link/.config/nvim/init.lua
+++ link/.config/nvim/init.lua
@@ -4,16 +4,26 @@ local o = vim.opt
 
 -- config
 o.clipboard:append({ unnamedeplus = true })
+o.mouse = 'a'
+o.visualbell = true
+o.emoji = true
+
+-- language
+vim.env.LANG = 'en_US.UTF-8'
 o.encoding = 'utf-8'
 o.fileencoding = 'utf-8'
-o.mouse = 'a'
-vim.env.LANG = 'en_US.UTF-8'
-
--- appearence
 o.ambiwidth = 'double'
-o.cmdheight = 1
-o.emoji = true
+
+-- files
 o.fileformats = "unix,dos,mac"
+
+-- workbench
+o.cmdheight = 1
+o.showtabline = 2
+
+-- editor
+--- render
+o.number = true
 o.list = true
 o.listchars = {
     space = '･',
@@ -23,15 +33,12 @@ o.listchars = {
     precedes = '«',
     nbsp = '+'
 }
+--- highlight
 vim.cmd [[ highlight NonText ctermfg=59 ctermbg=None guifg=None guibg=NONE ]]
 vim.cmd [[ highlight SpecialKey ctermfg=59 ctermbg=None guifg=None guibg=NONE ]]
-o.number = true
 o.showmatch = true
 o.matchtime = 1
-o.showtabline = 2
-o.visualbell = true
-
--- tab
+--- tab
 o.expandtab = true
 o.shiftwidth = 4
 o.smarttab = true
@@ -41,11 +48,12 @@ o.tabstop = 4
 -- keybind
 vim.g.mapleader = ' '
 
--- case
+-- search
 o.ignorecase = true
 o.smartcase = true
 o.wrapscan = true
 
+-- autocmd
 vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
     pattern = 'plugins.lua',
     command = 'PackerCompile',
