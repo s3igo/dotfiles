8ce1f27a262b5bb06bfe3059749084e92929f92a
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-09 17:55:35 +0900

    feat: zinit.shを分割

---
+++ lib/shell/plugin.sh
@@ -0,0 +1,58 @@
+# auto complete
+zinit light marlonrichert/zsh-autocomplete
+
+# theme
+zinit ice pick"async.zsh" src"pure.zsh"
+zinit light sindresorhus/pure
+
+# asdf
+zinit ice wait'!0'
+zinit light asdf-vm/asdf
+export ASDF_DATA_DIR="$HOME/.local/share/asdf"
+
+# autosuggestion
+zinit ice wait'!0'
+zinit light zsh-users/zsh-autosuggestions
+
+# tab-completion
+zinit ice wait'!0'
+zinit light zsh-users/zsh-completions
+
+# syntax-highlight
+zinit ice wait'!0'
+zinit light zsh-users/zsh-syntax-highlighting
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main line brackets cursor)
+typeset -A ZSH_HIGHLIGHT_STYLES
+ZSH_HIGHLIGHT_STYLES[alias]='fg=cyan,bold' # alias
+ZSH_HIGHLIGHT_STYLES[path]='fg=yellow,bold' # exist path
+ZSH_HIGHLIGHT_STYLES[root]='bg=red' # root
+
+# open on GitHub
+zinit ice wait'!0'
+zinit light paulirish/git-open
+
+# auto input closure/quote
+zinit ice wait'!0'
+zinit light momo-lab/zsh-smartinput
+
+# remove doller from the beginning of line
+zinit ice wait'!0'
+zinit light zpm-zsh/undollar
+
+# anyframe
+zinit ice wait'!0'
+zinit light mollifier/anyframe
+
+## cdr
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+zle -N anyframe-widget-cdr
+bindkey '^W' anyframe-widget-cdr
+
+## command history
+zle -N anyframe-widget-execute-history
+bindkey '^R' anyframe-widget-execute-history
+
+## ghq
+zle -N anyframe-widget-cd-ghq-repository
+bindkey '^G' anyframe-widget-cd-ghq-repository
--- link/.config/zsh/.zshrc
+++ link/.config/zsh/.zshrc
@@ -1,8 +1,8 @@
 SHELL_DIR=~/.dotfiles/lib/shell
 
 source $SHELL_DIR/zinit.sh
+source $SHELL_DIR/plugin.sh
 source $SHELL_DIR/config.sh
 source $SHELL_DIR/config_z.sh
 source $SHELL_DIR/aliases.sh
 source $SHELL_DIR/aliases_z.sh
-
