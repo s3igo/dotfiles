7e0a2505572cf9b76364ed55f11f50df6ae34f79
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-16 22:42:02 +0900

    change: update install command

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # ./bin/init.sh
-bash -c "$(curl -L raw.githubusercontent.com/s3igo/dotfiles/main/bin/init.sh)"
-
-# brew path
-[ "$(uname)" = 'Darwin' ] && [ "$(uname -m)" = 'arm64' ] && eval "$(/opt/homebrew/bin/brew shellenv)"
-[ "$(uname)" = 'Linux' ] && eval "$($HOME/.linuxbrew/bin/brew shellenv)"
+eval "$(curl -L raw.githubusercontent.com/s3igo/dotfiles/main/bin/init.sh)"
 
 # clone dotfiles
 git clone https://github.com/s3igo/dotfiles.git ~/.dotfiles
@@ -14,4 +10,4 @@ git clone https://github.com/s3igo/dotfiles.git ~/.dotfiles
 make -C ~/.dotfiles tool
 
 # ./bin/link.sh
-bash -c "$(curl -L raw.githubusercontent.com/s3igo/dotfiles/main/bin/link.sh)"
+eval "$(curl -L raw.githubusercontent.com/s3igo/dotfiles/main/bin/link.sh)"
