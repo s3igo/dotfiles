21c9c645055e7bd12e0187af5e2be775a1d88ad9
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-16 22:15:51 +0900

    add: linuxの場合の依存先のインストール処理

--- bin/init.sh
+++ bin/init.sh
@@ -1,25 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-# install Xcode-CLI
+# install dependencies
 [ "$(uname)" = 'Darwin' ] \
     && which xcode-select > /dev/null 2>&1 \
     || xcode-select --install
+if [ "$(uname)" = 'Linux' ]; then
+    which apt 2>&1 /dev/null
+    if [ $? -eq 0 ]; then
+        sudo apt -y update
+        sudo apt -y upgrade
+        sudo apt -y autoremove
+        sudo apt -y install build-essential
+    else
+        sudo yum -y update
+        sudo yum -y upgrade
+        sudo yum -y groupinstall "Development Tools"
+    fi
+fi
 
 # install brew
-which brew > /dev/null 2>&1 \
-    || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" \
-    && [ "$(uname -m)" = 'arm64' ] \
-    && eval "$(/opt/homebrew/bin/brew shellenv)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+[ "$(uname)" = 'Darwin' ] && [ "$(uname -m)" = 'arm64' ] && eval "$(/opt/homebrew/bin/brew shellenv)"
+[ "$(uname)" = 'Linux' ] && eval "$($HOME/.linuxbrew/bin/brew shellenv)"
 
-# brew update & upgrade
-which brew > /dev/null 2>&1 \
-    && brew update \
-    && brew upgrade
+brew update && brew upgrade
 
-# install git
+# install git & make
 which git > /dev/null 2>&1 || brew install git
-
-# install make
 which make > /dev/null 2>&1 || brew install make
 
 if [ "$SHELL" != '/bin/zsh' ]; then
