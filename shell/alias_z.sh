907460bdbbcd3361a88dc8b30a5d6e89db98721e
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-18 14:17:01 +0900

    change: update

--- shell/alias_z.sh
+++ shell/alias_z.sh
@@ -3,37 +3,39 @@ alias _start='source ~/.config/zsh/.zshrc'
 
 # global
 ## shell
-alias -g _head='cut -d " " -f 1'
-alias -g _i='install'
-alias -g _opt=anyframe-selector-auto
+alias -g @g='| grep'
+alias -g @head='| cut -d " " -f 1'
+alias -g @i='install'
 
 ## docker
-alias -g _dp='`docker ps --format "table {{.ID}} {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
-    | tail -n +2 | anyframe-selector-auto | cut -d " " -f 1`'
-alias -g _dp-a='`docker ps -a --format "table {{.ID}} {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
-    | tail -n +2 | anyframe-selector-auto | cut -d " " -f 1`'
+alias -g @dp='`docker ps --format "table {{.ID}} {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
+    | tail -n +2 | fzf | cut -d " " -f 1`'
+alias -g @dp-a='`docker ps -a --format "table {{.ID}} {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" \
+    | tail -n +2 | fzf | cut -d " " -f 1`'
+
+# keybind
+## cdr
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+zle -N anyframe-widget-cdr
+bindkey '^W' anyframe-widget-cdr
+
+## command history
+zle -N anyframe-widget-execute-history
+bindkey '^R' anyframe-widget-execute-history
+
+## ghq
+zle -N anyframe-widget-cd-ghq-repository
+bindkey '^G' anyframe-widget-cd-ghq-repository
 
 # mac
 if [ "$(uname)" = 'Darwin' ]; then
-    alias -g _pick='| anyframe-selector-auto | pbcopy'
+    ## global
+    alias -g _pick='| fzf | pbcopy'
     ## keybind
     function __open-app() {
-        open -a "$(ls /Applications | sed 's/\.app$//' | anyframe-selector-auto)"
+        open -a "$(ls /Applications | sed 's/\.app$//' | fzf)"
     }
     zle -N __open-app
     bindkey 'å' __open-app
-
-    ### cdr
-    autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
-    add-zsh-hook chpwd chpwd_recent_dirs
-    zle -N anyframe-widget-cdr
-    bindkey '^W' anyframe-widget-cdr
-
-    ### command history
-    zle -N anyframe-widget-execute-history
-    bindkey '^R' anyframe-widget-execute-history
-
-    ### ghq
-    zle -N anyframe-widget-cd-ghq-repository
-    bindkey '^G' anyframe-widget-cd-ghq-repository
 fi
