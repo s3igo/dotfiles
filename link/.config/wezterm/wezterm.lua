19aacf075cbb1086ed6330626d688ffa000deaed
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-20 14:24:05 +0900

    feat: ime入力中のカーソル

--- link/.config/wezterm/wezterm.lua
+++ link/.config/wezterm/wezterm.lua
@@ -1,11 +1,18 @@
 local wezterm = require 'wezterm';
 
+local scheme = wezterm.color.get_builtin_schemes()['NightOwl (Gogh)']
+local brightYellow = scheme.brights[4]
+
+scheme.compose_cursor = brightYellow
+scheme.cursor_fg = scheme.background
+
 return {
     font = wezterm.font('UDEV Gothic NFLG'),
     font_size = 18,
-    colors = {
-        compose_cursor = 'orange'
+    color_schemes = {
+        ['NightOwl (Gogh)'] = scheme,
     },
+    color_scheme = 'NightOwl (Gogh)',
     hide_tab_bar_if_only_one_tab = true,
     window_background_opacity = 0.7,
 }
