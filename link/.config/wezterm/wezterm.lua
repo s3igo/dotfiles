01199905122eddd826440125f1110e5bc85b983a
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-20 14:41:19 +0900

    change: cell_width

--- link/.config/wezterm/wezterm.lua
+++ link/.config/wezterm/wezterm.lua
@@ -9,10 +9,13 @@ scheme.cursor_fg = scheme.background
 return {
     font = wezterm.font('UDEV Gothic NFLG'),
     font_size = 18,
+    cell_width = 1.05,
+
     color_schemes = {
         ['NightOwl (Gogh)'] = scheme,
     },
     color_scheme = 'NightOwl (Gogh)',
-    hide_tab_bar_if_only_one_tab = true,
     window_background_opacity = 0.7,
+
+    hide_tab_bar_if_only_one_tab = true,
 }
