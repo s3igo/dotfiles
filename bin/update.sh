d4cb9418413aa6e571ffa8d95ca816ea97a6206b
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-22 20:59:47 +0900

    chore: format

--- bin/update.sh
+++ bin/update.sh
@@ -4,23 +4,23 @@ source ~/.dotfiles/var.sh
 
 # brew
 which brew > /dev/null 2>&1 \
-    && echo '--- update homebrew ---' \
+    && echo "\n--- update homebrew ---" \
     && brew update \
     && brew upgrade \
     && brew cleanup
 
 # mas-cli
 which mas > /dev/null 2>&1 \
-    && echo '--- update mas-cli ---' \
+    && echo "\n--- update mas-cli ---" \
     && mas upgrade
 
 # zinit
 source "$SHELL_DIR/zinit.sh"
 type zinit > /dev/null 2>&1 \
-    && echo '--- update zinit ---' \
+    && echo "\n--- update zinit ---" \
     && zinit update --all
 
 # asdf
 type asdf > /dev/null 2>&1 \
-    && echo '--- update asdf ---' \
+    && echo "\n--- update asdf ---" \
     && asdf plugin-update --all
