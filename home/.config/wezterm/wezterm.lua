8c1ac31be43f34c23c5bb16d89d25f97a0697515
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-23 11:17:50 +0900

    change: update

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -7,15 +7,23 @@ scheme.compose_cursor = brightYellow
 scheme.cursor_fg = scheme.background
 
 return {
+    -- appearance
+    color_schemes = { ['NightOwl (Gogh)'] = scheme, },
+    color_scheme = 'NightOwl (Gogh)',
+    window_background_opacity = 0.7,
+
+    -- config
+    hide_tab_bar_if_only_one_tab = true,
+
+    -- font
     font = wezterm.font('UDEV Gothic NFLG'),
     font_size = 18,
     cell_width = 1.05,
 
-    color_schemes = {
-        ['NightOwl (Gogh)'] = scheme,
+    -- keybind
+    disable_default_key_bindings = true,
+    keys = {
+        { key = 'w', mods = 'CMD', action = wezterm.action.CloseCurrentTab { confirm = false }, },
+        { key = 'q', mods = 'CMD', action = wezterm.action.QuitApplication },
     },
-    color_scheme = 'NightOwl (Gogh)',
-    window_background_opacity = 0.7,
-
-    hide_tab_bar_if_only_one_tab = true,
 }
