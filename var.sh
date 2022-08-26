56eec1356c4ecc63e1a8077bb969128a9477450c
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-26 14:26:22 +0900

    chore: format

--- var.sh
+++ var.sh
@@ -1,16 +1,15 @@
+# utils
 declare DOT_DIR="${HOME}/.dotfiles"
-
-declare LINK_DIR="${DOT_DIR}/home"
-declare SHELL_DIR="${DOT_DIR}/shell"
-
-PKG_DIR=~/.dotfiles/pkg # for makefile
-
-declare XDG_CONFIG_HOME="${HOME}/.config"
-declare XDG_CACHE_HOME="${HOME}/.cache"
-
 declare LOCAL_DIR="${HOME}/.local"
 
+# paths in dotfiles
+declare LINK_DIR="${DOT_DIR}/home"
+declare SHELL_DIR="${DOT_DIR}/shell"
+PKG_DIR=~/.dotfiles/pkg # for makefile
+
+# xdg base directories
+declare XDG_CONFIG_HOME="${HOME}/.config"
+declare XDG_CACHE_HOME="${HOME}/.cache"
 declare XDG_DATA_HOME="${LOCAL_DIR}/share"
 declare XDG_STATE_HOME="${LOCAL_DIR}/state"
 declare XDG_RUNTIME_DIR="${LOCAL_DIR}/runtime"
-
