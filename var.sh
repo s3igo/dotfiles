a83a284c6d6e29a4404a995f4cbe900fd8621d30
s3igo <85787242+s3igo@users.noreply.github.com>
2022-08-28 15:49:25 +0900

    fix: `PKG_DIR`変数にスペースが含まれる問題に対応

--- var.sh
+++ var.sh
@@ -5,7 +5,8 @@ declare LOCAL_DIR="${HOME}/.local"
 # paths in dotfiles
 declare LINK_DIR="${DOT_DIR}/home"
 declare SHELL_DIR="${DOT_DIR}/shell"
-PKG_DIR=~/.dotfiles/pkg # for makefile
+## for makefile
+PKG_DIR=~/.dotfiles/pkg
 
 # xdg base directories
 declare XDG_CONFIG_HOME="${HOME}/.config"
