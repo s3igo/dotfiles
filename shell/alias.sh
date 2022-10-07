120a76a0f463173163c86ab217c83f5d2b0c4401
s3igo <85787242+s3igo@users.noreply.github.com>
2022-10-07 15:35:23 +0900

    fix: `latest`, `dir` commands initialize stetement

--- shell/alias.sh
+++ shell/alias.sh
@@ -34,17 +34,18 @@ fi
 
 # functions
 function latest() {
-    local arg=${1:-$PWD}
+    declare arg=${1:-$PWD}
+    declare opt=''
     getopts 'l' opt
     if [[ "$opt" == 'l' ]]; then
         command ls -rtl "$arg" | tail -n 1
     else
         command ls -rt "$arg" | tail -n 1
-    # command ls -rt "$arg" | tail -n 1
     fi
 }
 
 function dir() {
+    declare opt=''
     getopts 'a' opt
     if [[ "$opt" == 'a' ]]; then
         cd "$(fd -HI -t d -E .git -E node_modules | fzf)"
