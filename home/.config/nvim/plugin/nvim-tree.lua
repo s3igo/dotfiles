638618297db69835343ce6eab089efbd5dd9815a
s3igo <85787242+s3igo@users.noreply.github.com>
2022-10-22 21:23:37 +0900

    add: nvim-tree, telescope settings

---
+++ home/.config/nvim/plugin/nvim-tree.lua
@@ -0,0 +1,12 @@
+local status, nvim_tree = pcall(require, 'nvim-tree')
+if (not status) then return end
+
+local map = vim.keymap
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+nvim_tree.setup({
+    -- hijack_netrw = true,
+})
+
+map.set('n', '<leader>b', ':NvimTreeToggle<cr>')
---
+++ home/.config/nvim/plugin/telescope.lua
@@ -0,0 +1,37 @@
+local status, telescope = pcall(require, 'telescope')
+if (not status) then return end
+
+local builtin = require('telescope.builtin')
+local map = vim.keymap
+
+telescope.setup {
+    defaults = {
+        file_ignore_patterns = { '^./.git/', '^node_modules/' },
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+        },
+        file_browser = {
+            hijack_netrw = true,
+        },
+    }
+}
+
+-- telescope.load_extension('file_browser')
+
+map.set('n', '<leader>f', function()
+    builtin.find_files({
+        no_ignore = false,
+        hidden = true,
+    })
+end)
+
+--[[ map.set('n', 'sf', function()
+    telescope.extensions.file_browser.file_browser({
+        hidden = true,
+    })
+end) ]]
