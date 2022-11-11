acc7bacd18458b7afa8e10d898cf27d5b88ea7fd
s3igo <85787242+s3igo@users.noreply.github.com>
2022-11-11 22:13:12 +0900

    change: remove asdf

--- shell/config.sh
+++ shell/config.sh
@@ -14,11 +14,6 @@ set -o noclobber
 [[ "$(uname)" == 'Darwin' ]] && [[ "$(uname -m)" == 'arm64' ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
 [[ "$(uname)" == 'Linux' ]] && eval "$(${HOME}/.linuxbrew/bin/brew shellenv)"
 
-# asdf
-declare -x ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/asdfrc"
-declare -x ASDF_DATA_DIR="${XDG_DATA_HOME}/asdf"
-source "$(brew --prefix asdf)/libexec/asdf.sh"
-
 # terminfo
 declare -x TERMINFO="${XDG_DATA_HOME}/terminfo"
 declare -x TERMINFO_DIRS="${XDG_DATA_HOME}/terminfo:/usr/share/terminfo"
@@ -31,6 +26,7 @@ declare -x JUPYTER_CONFIG_DIR="${XDG_CONFIG_HOME}/jupyter"
 # node
 declare -x NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
 declare -x NODE_REPL_HISTORY="${XDG_DATA_HOME}/node_repl_history"
+export PATH="$PATH:/usr/local/opt/node@16/bin"
 
 # lesshist
 declare -x LESSHISTFILE="${XDG_CACHE_HOME}/less/history"
@@ -38,3 +34,6 @@ declare -x LESSHISTFILE="${XDG_CACHE_HOME}/less/history"
 # editor
 declare -x EDITOR='nvim'
 bindkey -e # explicit use emacs keybind
+
+# zk
+declare -x ZK_NOTEBOOK_DIR="${HOME}/src/github.com/s3igo/notes"
