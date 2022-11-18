f03f9e4c100a54aba5791bfd77064b6e6508ea24
s3igo <85787242+s3igo@users.noreply.github.com>
2022-11-18 15:19:10 +0900

    change: update init operation

--- bin/init.sh
+++ bin/init.sh
@@ -1,44 +1,29 @@
 #!/bin/bash
 
-# install dependencies
-[[ "$(uname)" == 'Darwin' ]] \
-    && type xcode-select > /dev/null 2>&1 \
-    || xcode-select --install
-if [[ "$(uname)" == 'Linux' ]]; then
-    if type apt > /dev/null 2>&1; then
-        sudo apt -y update
-        sudo apt -y upgrade
-        sudo apt -y autoremove
-        sudo apt -y install build-essential
-    else
-        sudo yum -y update
-        sudo yum -y upgrade
-        sudo yum -y groupinstall 'Development Tools'
-    fi
+# set package manager
+if [[ "$(uname)" == 'Darwin' ]]; then
+     type xcode-select > /dev/null 2>&1 || xcode-select --install
+     type brew > /dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+     [[ "$(uname -m)" == 'arm64' ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-# install brew
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-[[ "$(uname)" == 'Darwin' ]] && [[ "$(uname -m)" == 'arm64' ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
-[[ "$(uname)" == 'Linux' ]] && eval "$($HOME/.linuxbrew/bin/brew shellenv)"
 
-# install git & make
-type git > /dev/null 2>&1 || brew install git
-type make > /dev/null 2>&1 || brew install make
-
-if [[ "$SHELL" != *zsh ]]; then
-    # install zsh
-    type zsh > /dev/null 2>&1 \
-        || brew install zsh \
-        && which zsh >> /etc/shells
-
-    # change shell to zsh
-    chsh -s `which zsh`
+# init package manager
+if type brew > /dev/null 2>&1; then
+    sudo brew update && sudo brew upgrade
+elif type apt > /dev/null 2>&1; then
+    sudo apt update && sudo apt upgrade
+elif type yum > /dev/null 2>&1; then
+    sudo yum update && sudo yum upgrade
+else
+    echo 'unknown package manager'
+    exit 1
 fi
 
-# init zsh
-echo 'export ZDOTDIR="${HOME}/.config/zsh"' | sudo tee -a /etc/zshenv
+# init zsh 
+# NOTE: mutable operation
+[[ "$SHELL" == *zsh ]] && echo 'export ZDOTDIR="${HOME}/.config/zsh"' | sudo tee -a /etc/zshenv
 
-# make default xdg directories
+# default xdg-based directories
 mkdir -p ~/{.config,.cache}
 mkdir -p ~/.local/{share,state}
---
+++ spec.md
@@ -0,0 +1,14 @@
+# dotfiles specification
+
+## dependencies
+
+- git
+- make
+- curl
+
+macOSについて、これらはデフォルトでインストールされている
+Linuxについても多くのパッケージマネージャでビルドなしに十分新しいバイナリを入手可能
+
+## shell
+
+zsh/bashに関して、環境変数`$SHELL`によって処理を分岐する
