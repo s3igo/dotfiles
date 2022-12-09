f179bdb82235bf0fda1e70529cbf5fe3bb71284b
s3igo <85787242+s3igo@users.noreply.github.com>
2022-12-09 15:39:51 +0900

    remove: devcontainer command

--- bin/devcontainer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[[ "$SHELL" == *bash ]] \
-    && ln -fnsv ~/.dotfiles/home/.bashrc ~/.bashrc \
-    && ln -fnsv ~/.dotfiles/home/.bash_profile ~/.bash_profile
-
-ln -fnsv ~/.dotfiles/home/.vimrc ~/.vimrc
