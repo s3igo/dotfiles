cf01592585f5469c0fe7ee279a7e8b11207c5ba1
s3igo <85787242+s3igo@users.noreply.github.com>
2022-12-10 20:02:08 +0900

    remove: vscode config

--- bin/link.sh
+++ bin/link.sh
@@ -2,24 +2,6 @@
 
 declare LINK_DIR="${HOME}/.dotfiles/home"
 
-# `uname`によって分岐
-function vscode {
-    local DIRNAME="$(dirname "$FILE")"
-
-    [[ "$(basename "$DIRNAME")" != '_vscode' ]] && return 1
-
-    if [[ "$(uname)" == 'Darwin' ]]; then
-        mkdir -p "${HOME}/Library/Application Support/Code/User"
-        ln -fnsv "$FILE" "${HOME}/Library/Application Support/Code/User/$(basename "$FILE")"
-    else
-        mkdir -p "${HOME}/.config/Code/User"
-        ln -fnsv "$FILE" "${HOME}/.config/Code/User/$(basename "$FILE")"
-    fi
-
-    return 0
-}
-
-# `$SHELL`によって分岐
 function shell {
     [[ "$(basename "$FILE")" == .bash* ]] && [[ "$SHELL" == *zsh ]] && return 0
 
@@ -30,7 +12,6 @@ function shell {
 
 # make file-based symlink
 while read -r FILE; do
-    vscode && continue
     shell && continue
 
     declare DEST="${HOME}${FILE##"$LINK_DIR"}"
