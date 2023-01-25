afba6c7ed7d2b1d266597a27854ea951ea9f41e1
s3igo <85787242+s3igo@users.noreply.github.com>
2023-01-25 10:50:09 +0900

    change: plugin依存の設定を移動

--- home/.config/zsh/src/config_z.sh
+++ home/.config/zsh/src/config_z.sh
@@ -36,7 +36,6 @@ alias -g @d_ps='"$(docker ps | tail -n +2 | fzf | read ID IMAGE COMMAND CREATED 
 alias -g @d_ps-a='"$(docker ps -a | tail -n +2 | fzf | read ID IMAGE COMMAND CREATED STATUS PORTS NAMES && echo $ID)"'
 alias -g @d_images='"$(docker images | tail -n +2 | fzf | read REPOSITORY TAG IMAGEID CREATED SIZE && echo $IMAGEID)"'
 
-
 ## mac
 if [[ "$(uname)" == 'Darwin' ]]; then
     alias -g @cp='| pbcopy'
@@ -45,25 +44,4 @@ fi
 
 # keybind
 bindkey -e # explicit use emacs keybind
-
 bindkey '^U' backward-kill-line
-bindkey '^J' menu-select
-
-## cdr
-autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
-add-zsh-hook chpwd chpwd_recent_dirs
-zle -N anyframe-widget-cdr
-bindkey '^S' anyframe-widget-cdr
-
-## command history
-zle -N anyframe-widget-execute-history
-bindkey '^R' anyframe-widget-execute-history
-
-## ghq
-function __ghq-fzf {
-    anyframe-source-ghq-repository \
-        | fzf --preview "command exa --tree --git-ignore -I 'node_modules|.git' {}" \
-        | anyframe-action-execute cd --
-}
-zle -N __ghq-fzf
-bindkey '^G' __ghq-fzf
