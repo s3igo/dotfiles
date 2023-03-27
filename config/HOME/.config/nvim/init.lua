6fe916ee3cdf092eec7930ff4a9b04d3c4ffa4dc
s3igo <85787242+s3igo@users.noreply.github.com>
2023-03-27 17:57:21 +0900

    change: remove plugins and format

--- config/HOME/.config/nvim/init.lua
+++ config/HOME/.config/nvim/init.lua
@@ -55,24 +55,28 @@ vim.keymap.set('n', '<C-w><C-n>', '<cmd>bn<cr>')
 vim.keymap.set('n', '<C-w>p', '<cmd>bp<cr>')
 vim.keymap.set('n', '<C-w><C-p>', '<cmd>bp<cr>')
 
---misc
+-- misc
 vim.keymap.set('n', 'Y', 'y$')
 vim.keymap.set('n', 'sd', '"_d')
 vim.keymap.set('i', '<C-s>', '<C-d>')
 
 -- emacs like keybindings
-vim.keymap.set({ 'c', 'i' }, '<C-a>', '<Home>')
-vim.keymap.set({ 'c', 'i' }, '<C-e>', '<End>')
-vim.keymap.set({ 'c', 'i' }, '<C-b>', '<Left>')
-vim.keymap.set({ 'c', 'i' }, '<C-f>', '<Right>')
-vim.keymap.set({ 'c', 'i' }, '<C-d>', '<Del>')
+vim.keymap.set({'c', 'i'}, '<C-a>', '<Home>')
+vim.keymap.set({'c', 'i'}, '<C-e>', '<End>')
+vim.keymap.set({'c', 'i'}, '<C-b>', '<Left>')
+vim.keymap.set({'c', 'i'}, '<C-f>', '<Right>')
+vim.keymap.set({'c', 'i'}, '<C-d>', '<Del>')
 vim.keymap.set('i', '<C-p>', '<Up>')
 vim.keymap.set('i', '<C-n>', '<Down>')
 vim.keymap.set('i', '<C-k>', '<Esc>lDa')
 vim.keymap.set('c', '<C-h>', '<BS>')
 
+-- bufferline
+vim.keymap.set('n', 's.', '<cmd>BufferLineMoveNext<cr>')
+vim.keymap.set('n', 's,', '<cmd>BufferLineMovePrev<cr>')
+
 -- --------------------------------- autocmd -------------------------------- --
-vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+vim.api.nvim_create_autocmd({'BufWritePost'}, {
     pattern = 'init.lua',
     command = 'PackerCompile'
 })
@@ -88,14 +92,8 @@ vim.cmd([[
 local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = vim.fn.system({
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-    })
+    PACKER_BOOTSTRAP = vim.fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim",
+                                      install_path})
     print("Installing packer")
     vim.cmd([[packadd packer.nvim]])
 end
@@ -109,35 +107,104 @@ vim.cmd([[
 ]])
 
 require('packer').startup(function(use)
-    use({ 'wbthomason/packer.nvim' })
-    use({ 'nvim-lua/plenary.nvim' })
-    use({ 'kyazdani42/nvim-web-devicons' })
-    use({ 'vim-jp/vimdoc-ja' })
-    use({ 'github/copilot.vim' })
-    use({ 'cappyzawa/trim.nvim' })
+    use({'wbthomason/packer.nvim'})
+    use({'nvim-lua/plenary.nvim'})
+    use({'kyazdani42/nvim-web-devicons'})
+    use({'vim-jp/vimdoc-ja'})
+    use({'github/copilot.vim'})
+    use({'cappyzawa/trim.nvim'})
     use({
-        'bluz71/vim-nightfly-guicolors' ,
-        config = function() vim.cmd('colorscheme nightfly') end
+        'bluz71/vim-nightfly-guicolors',
+        config = function()
+            vim.cmd('colorscheme nightfly')
+        end
     })
-    use({ 'lewis6991/gitsigns.nvim' })
-    use({ 'lukas-reineke/indent-blankline.nvim' })
-    use({ 'nvim-lualine/lualine.nvim' })
-    use({ 'b3nj5m1n/kommentary' })
+    use({
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup({
+                signs = {
+                    add = {
+                        hl = 'GitSignsAdd',
+                        text = '|',
+                        numhl = 'GitSignsAddNr',
+                        linehl = 'GitSignsAddLn'
+                    },
+                    change = {
+                        hl = 'GitSignsChange',
+                        text = '|',
+                        numhl = 'GitSignsChangeNr',
+                        linehl = 'GitSignsChangeLn'
+                    },
+                    delete = {
+                        hl = 'GitSignsDelete',
+                        text = '_',
+                        numhl = 'GitSignsDeleteNr',
+                        linehl = 'GitSignsDeleteLn'
+                    },
+                    topdelete = {
+                        hl = 'GitSignsDelete',
+                        text = '‾',
+                        numhl = 'GitSignsDeleteNr',
+                        linehl = 'GitSignsDeleteLn'
+                    },
+                    changedelete = {
+                        hl = 'GitSignsChange',
+                        text = '~',
+                        numhl = 'GitSignsChangeNr',
+                        linehl = 'GitSignsChangeLn'
+                    }
+                },
+                signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+                numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+                linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+                word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+                watch_gitdir = {
+                    interval = 1000,
+                    follow_files = true
+                },
+                attach_to_untracked = true,
+                current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+                current_line_blame_opts = {
+                    virt_text = true,
+                    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                    delay = 1000,
+                    ignore_whitespace = false
+                },
+                current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+                sign_priority = 6,
+                update_debounce = 100,
+                status_formatter = nil, -- Use default
+                max_file_length = 40000, -- Disable if file is longer than this (in lines)
+                preview_config = {
+                    -- Options passed to nvim_open_win
+                    border = 'single',
+                    style = 'minimal',
+                    relative = 'cursor',
+                    row = 0,
+                    col = 1
+                },
+                yadm = {
+                    enable = false
+                }
+            })
+        end
+    })
+    use({'lukas-reineke/indent-blankline.nvim'})
+    use({'nvim-lualine/lualine.nvim'})
+    use({'b3nj5m1n/kommentary'})
     use({
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate',
-        requires = {
-            'windwp/nvim-ts-autotag',
-            'nvim-treesitter/nvim-treesitter-context',
-        },
+        requires = {'windwp/nvim-ts-autotag', 'nvim-treesitter/nvim-treesitter-context'}
     })
     use({
         'nvim-telescope/telescope-frecency.nvim',
-        requires = 'kkharji/sqlite.lua',
+        requires = 'kkharji/sqlite.lua'
     })
     use({
         'nvim-telescope/telescope-fzf-native.nvim',
-        run = 'make',
+        run = 'make'
     })
     use({
         'nvim-telescope/telescope.nvim',
@@ -145,30 +212,53 @@ require('packer').startup(function(use)
     })
     use({
         'norcalli/nvim-colorizer.lua',
-        config = function() require('colorizer').setup() end
+        config = function()
+            require('colorizer').setup()
+        end
     })
     use({
         'windwp/nvim-autopairs',
-        config = function() require('nvim-autopairs').setup() end
+        config = function()
+            require('nvim-autopairs').setup()
+        end
     })
-    use({ 'akinsho/bufferline.nvim' })
+    use({
+        'akinsho/bufferline.nvim',
+        config = function()
+            require('bufferline').setup({
+                options = {
+                    numbers = 'ordinal',
+                    buffer_close_icon = '',
+                    close_icon = '',
+                    diagnostics = 'nvim_lsp',
+                    separator_style = {'', ''}
+                }
+            })
+        end
+    })
     use({
         'kylechui/nvim-surround',
-        config = function() require('nvim-surround').setup() end
+        config = function()
+            require('nvim-surround').setup()
+        end
     })
-    use({ 'nvim-tree/nvim-tree.lua' })
+    use({'nvim-tree/nvim-tree.lua'})
 
     -- completion
-    use({ 'kevinhwang91/nvim-hlslens' })
+    use({'kevinhwang91/nvim-hlslens'})
     use {
         "tversteeg/registers.nvim",
-        config = function() require("registers").setup() end,
+        config = function()
+            require("registers").setup()
+        end
     }
     use({
         "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
+        run = function()
+            vim.fn["mkdp#util#install"]()
+        end
     })
-    use({ 'mickael-menu/zk-nvim' })
+    use({'mickael-menu/zk-nvim'})
 end)
 
 -- ------------------------------- appearance ------------------------------- --
