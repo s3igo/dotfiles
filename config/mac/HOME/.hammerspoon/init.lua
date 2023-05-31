7bf22e8af85b75cb96bcc358d6d3f850e5b54589
s3igo <85787242+s3igo@users.noreply.github.com>
2023-05-31 15:11:46 +0900

    feat: key remap

--- config/mac/HOME/.hammerspoon/init.lua
+++ config/mac/HOME/.hammerspoon/init.lua
@@ -1,10 +1,43 @@
 local map = hs.keycodes.map
 local keyDown = hs.eventtap.event.types.keyDown
 local flagsChanged = hs.eventtap.event.types.flagsChanged
-local keyStroke = hs.eventtap.keyStroke
+local function keyStroke(mods, char, delay)
+    delay = delay or 0
+    hs.eventtap.keyStroke(mods, char, delay)
+end
 
+local function remap(mods, key, func)
+    return hs.hotkey.bind(mods, key, func, nil, func)
+end
+
+local remapKeys = {
+    remap({'ctrl'}, '[', function() keyStroke({}, 'escape') end),
+    remap({'ctrl'}, 'h', function() keyStroke({}, 'delete') end),
+    remap({'ctrl'}, 'm', function() keyStroke({}, 'return') end),
+    remap({'ctrl'}, 'u', function() keyStroke({'cmd'}, 'delete') end),
+    remap({'ctrl'}, 'w', function() keyStroke({'alt'}, 'delete') end),
+    remap({'rightalt'}, 'f', function() keyStroke({'alt'}, 'right') end),
+    remap({'rightalt'}, 'b', function() keyStroke({'alt'}, 'left') end),
+}
+
+local function handleGlobalAppEvent(name, event, app)
+    if event == hs.application.watcher.activated then
+        if name == 'Alacritty' or name == 'Code' then
+            for i, key in ipairs(remapKeys) do
+                key:disable()
+            end
+        else
+            for i, key in ipairs(remapKeys) do
+                key:enable()
+            end
+        end
+    end
+end
+appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+appsWatcher:start()
+
+-- left/right_cmd to eisuu/kana
 local isCmdAsModifier = false
-
 local function switchInputSourceEvent(event)
     local eventType = event:getType()
     local keyCode = event:getKeyCode()
@@ -28,6 +61,5 @@ local function switchInputSourceEvent(event)
         end
     end
 end
-
 eventTap = hs.eventtap.new({keyDown, flagsChanged}, switchInputSourceEvent)
 eventTap:start()
