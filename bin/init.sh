a1783bc60f62570c6bef8621692ca65a24f07675
s3igo <85787242+s3igo@users.noreply.github.com>
2023-06-02 21:52:43 +0900

    change: remove redundant process

--- bin/init.sh
+++ bin/init.sh
@@ -7,19 +7,6 @@ if [[ "$(uname)" == 'Darwin' ]]; then
      [[ "$(uname -m)" == 'arm64' ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-
-# init package manager
-# if type brew > /dev/null 2>&1; then
-#     sudo brew update && sudo brew upgrade
-# elif type apt > /dev/null 2>&1; then
-#     sudo apt update && sudo apt upgrade
-# elif type yum > /dev/null 2>&1; then
-#     sudo yum update && sudo yum upgrade
-# else
-#     echo 'unknown package manager'
-#     exit 1
-# fi
-
 # default xdg-based directories
 mkdir -p ~/{.config,.cache}
 mkdir -p ~/.local/{share,state}
