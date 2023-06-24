c679591fe5bc6df993898b7e379cd37546f8d21c
s3igo <85787242+s3igo@users.noreply.github.com>
2023-06-24 09:40:16 +0900

    fix: exit successfully

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,3 +8,8 @@ git clone https://github.com/s3igo/dotfiles.git ~/.dotfiles
 
 # scripts/link.sh
 eval "$(curl -fsSL raw.githubusercontent.com/s3igo/dotfiles/main/scripts/link.sh)"
+
+# restart shell
+exec $SHELL -l
+
+return 0
