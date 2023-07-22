e6d81d970fe8d276c73c2fe9b9891f57c0312b65
s3igo <85787242+s3igo@users.noreply.github.com>
2023-07-22 17:14:32 +0900

    add: nvim tree

--- config/common/HOME/.config/nvim/lua/plugins.lua
+++ config/common/HOME/.config/nvim/lua/plugins.lua
@@ -1,13 +1,34 @@
-return {
-    { -- colorscheme
-        'bluz71/vim-nightfly-guicolors',
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd('colorscheme nightfly')
-        end,
-    },
-    {
+return {{ -- colorscheme
+    'bluz71/vim-nightfly-guicolors',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd('colorscheme nightfly')
+    end
+}, { -- filer
+    'nvim-tree/nvim-tree.lua',
+    config = function()
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+        vim.keymap.set('n', 'sb', ':NvimTreeToggle<CR>')
+        vim.cmd('highlight NvimTreeNormal ctermbg=NONE guibg=NONE')
+        require('nvim-tree').setup({
+            view = {
+                mappings = {
+                    list = {{
+                        key = 's',
+                        action = ''
+                    }, {
+                        key = 'so',
+                        action = 'system_open'
+                    }}
+                }
+            },
+            filters = {
+                custom = {'\\.git$', '.cache$'}
+            }
+        })
+    end
+}, {
 
-    }
-}
+}}
