527d4c79864085fe6ecb698209d0720114b0e58c
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-18 18:03:24 +0900

    feat: transparent lualine

--- config/common/HOME/.config/nvim/lua/plugins.lua
+++ config/common/HOME/.config/nvim/lua/plugins.lua
@@ -338,44 +338,149 @@ return {{ -- colorscheme
 }, { -- status bar
     'nvim-lualine/lualine.nvim',
     event = 'VeryLazy',
-    opts = {
-        options = {
-            icons_enabled = false,
-            globalstatus = true,
-            component_separators = {
-                left = '',
-                right = ''
-            },
-            section_separators = {
-                left = '',
-                right = ''
-            }
-        },
-        sections = {
-            lualine_x = {'location'},
-            lualine_y = {{
-                'bo:expandtab',
-                fmt = function(str)
-                    if str == 'true' then
-                        return 'spaces:'
-                    else
-                        return 'tab size:'
-                    end
-                end
-            }, 'bo:tabstop'},
-            lualine_z = {'encoding', {
-                'fileformat',
-                fmt = function(str)
-                    local formats = {
-                        unix = 'LF',
-                        dos = 'CRLF',
-                        mac = 'CR'
-                    }
-                    return formats[str]
-                end
-            }, 'filetype'}
+    opts = function()
+        local colors = {
+            darkgray = "#16161d",
+            gray = "#727169",
+            innerbg = nil,
+            outerbg = "#16161D",
+            normal = "#7e9cd8",
+            insert = "#98bb6c",
+            visual = "#ffa066",
+            replace = "#e46876",
+            command = "#e6c384"
         }
-    }
+        return {
+            options = {
+                theme = {
+                    inactive = {
+                        a = {
+                            fg = colors.gray,
+                            bg = colors.outerbg,
+                            gui = "bold"
+                        },
+                        b = {
+                            fg = colors.gray,
+                            bg = colors.outerbg
+                        },
+                        c = {
+                            fg = colors.gray,
+                            bg = colors.innerbg
+                        }
+                    },
+                    visual = {
+                        a = {
+                            fg = colors.darkgray,
+                            bg = colors.visual,
+                            gui = "bold"
+                        },
+                        b = {
+                            fg = colors.gray,
+                            bg = colors.outerbg
+                        },
+                        c = {
+                            fg = colors.gray,
+                            bg = colors.innerbg
+                        }
+                    },
+                    replace = {
+                        a = {
+                            fg = colors.darkgray,
+                            bg = colors.replace,
+                            gui = "bold"
+                        },
+                        b = {
+                            fg = colors.gray,
+                            bg = colors.outerbg
+                        },
+                        c = {
+                            fg = colors.gray,
+                            bg = colors.innerbg
+                        }
+                    },
+                    normal = {
+                        a = {
+                            fg = colors.darkgray,
+                            bg = colors.normal,
+                            gui = "bold"
+                        },
+                        b = {
+                            fg = colors.gray,
+                            bg = colors.outerbg
+                        },
+                        c = {
+                            fg = colors.gray,
+                            bg = colors.innerbg
+                        }
+                    },
+                    insert = {
+                        a = {
+                            fg = colors.darkgray,
+                            bg = colors.insert,
+                            gui = "bold"
+                        },
+                        b = {
+                            fg = colors.gray,
+                            bg = colors.outerbg
+                        },
+                        c = {
+                            fg = colors.gray,
+                            bg = colors.innerbg
+                        }
+                    },
+                    command = {
+                        a = {
+                            fg = colors.darkgray,
+                            bg = colors.command,
+                            gui = "bold"
+                        },
+                        b = {
+                            fg = colors.gray,
+                            bg = colors.outerbg
+                        },
+                        c = {
+                            fg = colors.gray,
+                            bg = colors.innerbg
+                        }
+                    }
+                },
+                icons_enabled = false,
+                globalstatus = true,
+                component_separators = {
+                    left = '',
+                    right = ''
+                },
+                section_separators = {
+                    left = '',
+                    right = ''
+                }
+            },
+            sections = {
+                lualine_x = {'location'},
+                lualine_y = {{
+                    'bo:expandtab',
+                    fmt = function(str)
+                        if str == 'true' then
+                            return 'spaces:'
+                        else
+                            return 'tab size:'
+                        end
+                    end
+                }, 'bo:tabstop'},
+                lualine_z = {'encoding', {
+                    'fileformat',
+                    fmt = function(str)
+                        local formats = {
+                            unix = 'LF',
+                            dos = 'CRLF',
+                            mac = 'CR'
+                        }
+                        return formats[str]
+                    end
+                }, 'filetype'}
+            }
+        }
+    end
 }, { -- indent guides
     'lukas-reineke/indent-blankline.nvim',
     event = {'BufReadPost', 'BufNewFile'},
