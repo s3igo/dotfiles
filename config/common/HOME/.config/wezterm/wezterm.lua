a353904564b45eb9b1250b53c8a441da48a25e1a
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-26 14:12:42 +0900

    wip: wezterm config

--- config/common/HOME/.config/wezterm/wezterm.lua
+++ config/common/HOME/.config/wezterm/wezterm.lua
@@ -2,32 +2,98 @@ local wezterm = require('wezterm')
 local act = wezterm.action
 
 wezterm.on('update-right-status', function(window)
-    local str = ''
-    local table = window:active_key_table()
-    if table then
-        str = table .. '  '
-        -- elseif window:leader_is_active() then
-        --     str = 'leader' .. '  '
-    end
-    window:set_right_status(str)
+    local _, user = wezterm.run_child_process({ 'whoami' })
+    local name = user:gsub('\n', '') .. '@' .. wezterm.hostname()
+
+    local mode = window:active_key_table() or ''
+
+    window:set_right_status(wezterm.format({
+        { Background = { Color = 'blue' } },
+        { Text = mode },
+        'ResetAttributes',
+        { Text = '  ' .. name },
+    }))
 end)
 
+local font_name = 'UDEV Gothic NFLG'
+local scheme_name = 'NightOwl (Gogh)'
+local scheme = wezterm.get_builtin_color_schemes()[scheme_name]
+
+-- Set transparent tab bar by `text_background_opacity`, the fg and bg colors of the powerline glyph do not match.
+local transparent_bg = (function(theme)
+    local r, g, b = wezterm.color.parse(theme.background):srgba_u8()
+    return 'rgba(' .. r .. ', ' .. g .. ', ' .. b .. ', 0.7)'
+end)(scheme)
+
+local glyph = {
+    solid_right_arrow = utf8.char(0xe0b0),
+    right_arrow = utf8.char(0xe0b1),
+    solid_left_arrow = utf8.char(0xe0b2),
+    left_arrow = utf8.char(0xe0b3),
+}
+
+local function tab_title(tab_info)
+    local title = tab_info.tab_title
+    -- if the tab title is explicitly set, take that
+    if title and #title > 0 then
+        return title
+    end
+    -- Otherwise, use the title from the active pane
+    -- in that tab
+    return tab_info.active_pane.title
+end
+
 return {
+    term = 'wezterm',
+    scrollback_lines = 10000,
+
     -- Appearance
-    color_scheme = 'NightOwl (Gogh)',
+    color_scheme = scheme_name,
     foreground_text_hsb = { saturation = 1.05, brightness = 1.1 },
 
     -- color_scheme = 'Argonaut (Gogh)',
     -- foreground_text_hsb = { saturation = 0.95, brightness = 1.1 },
 
-    window_decorations = 'INTEGRATED_BUTTONS|RESIZE',
+    inactive_pane_hsb = { saturation = 0.6, brightness = 0.3 },
+
+    window_decorations = 'TITLE | RESIZE',
     window_background_opacity = 0.7,
-    inactive_pane_hsb = { saturation = 1, brightness = 1 },
+    window_frame = {
+        font = wezterm.font({ family = font_name, weight = 'Bold' }),
+        -- font_size = 14,
+        -- window_hide = false,
+    },
+
+    use_fancy_tab_bar = false,
+    show_new_tab_button_in_tab_bar = false,
+
+    -- colors = { tab_bar = { background = transparent_bg } },
+    -- or
+    text_background_opacity = 0.7,
+    colors = { tab_bar = { background = scheme.background } },
+
+    -- overwrite defaults
+    -- colors = {
+    --     tab_bar = (function(bg, fg, background)
+    --         local elem = {
+    --             bg_color = bg,
+    --             fg_color = fg,
+    --         }
+    --         return {
+    --             background = background,
+    --             active_tab = elem,
+    --             inactive_tab = elem,
+    --             inactive_tab_hover = elem,
+    --         }
+    --     end)(scheme.background, scheme.foreground, transparent_bg),
+    -- },
 
     -- font
-    font = wezterm.font('UDEV Gothic NFLG'),
+    font = wezterm.font(font_name),
     font_size = 16,
     line_height = 0.9,
+
+    window_padding = { top = 0, bottom = 0 },
 
     -- Keybind
     -- disable_default_key_bindings = true,
