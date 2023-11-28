0a23947e9dc0e9ff2835bd7c64ed8161dd66040a
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-28 12:30:04 +0900

    refactor: comment/var name

--- config/common/HOME/.config/wezterm/colors.lua
+++ config/common/HOME/.config/wezterm/colors.lua
@@ -5,24 +5,24 @@ local function modify_alpha(color, a)
     return wezterm.color.from_hsla(h, s, l, a)
 end
 
-local scheme = wezterm.get_builtin_color_schemes()['NightOwl (Gogh)']
+local default = wezterm.get_builtin_color_schemes()['NightOwl (Gogh)']
 
 -- NOTE: Set transparent tab bar by `text_background_opacity`,
 -- the fg and bg colors of the powerline glyph do not match.
-local transparent_bg = modify_alpha(scheme.background, 0.7)
+local bg = modify_alpha(default.background, 0.7)
 
-local colors = scheme
-colors.cursor_fg = 'black'
-colors.tab_bar = {
-    background = transparent_bg,
+local scheme = default
+scheme.cursor_fg = 'black'
+scheme.tab_bar = {
+    background = bg,
     inactive_tab_hover = {
-        bg_color = scheme.background,
-        fg_color = scheme.foreground,
+        bg_color = default.background,
+        fg_color = default.foreground,
     },
 }
 
 return {
-    default = scheme,
-    scheme = colors,
-    bg = transparent_bg,
+    default = default,
+    scheme = scheme,
+    bg = bg,
 }
--- config/common/HOME/.config/wezterm/wezterm.lua
+++ config/common/HOME/.config/wezterm/wezterm.lua
@@ -12,7 +12,7 @@ return {
     macos_forward_to_ime_modifier_mask = 'SHIFT | CTRL',
     send_composed_key_when_right_alt_is_pressed = false,
 
-    -- Appearance
+    -- appearance
     colors = colors.scheme,
     foreground_text_hsb = { saturation = 1.05, brightness = 1.1 },
 
@@ -28,12 +28,12 @@ return {
     show_new_tab_button_in_tab_bar = false,
     tab_max_width = 24,
 
-    -- Font
+    -- font
     font = font,
     font_size = 16,
     line_height = 0.9,
 
-    -- Keybind
+    -- keybind
     -- disable_default_key_bindings = true,
     -- NOTE: To use the key repeat by `one_shot = false`, the built-in leader key is not used.
     keys = {
@@ -51,7 +51,7 @@ return {
         leader = {
             { key = 's', mods = 'CTRL', action = 'PopKeyTable' },
 
-            -- Rename tab
+            -- rename tab
             {
                 key = 'r',
                 action = act.PromptInputLine({
@@ -64,11 +64,11 @@ return {
                 }),
             },
 
-            -- Split
+            -- split
             { key = 'v', action = act.SplitHorizontal },
             { key = 's', action = act.SplitVertical },
 
-            -- Focus
+            -- focus
             {
                 key = 'h',
                 mods = 'CTRL',
@@ -93,19 +93,19 @@ return {
             { key = 'n', mods = 'CTRL', action = act.ActivateTabRelative(1) },
             { key = 'p', mods = 'CTRL', action = act.ActivateTabRelative(-1) },
 
-            -- Resize
+            -- resize
             { key = 'h', mods = 'SHIFT', action = act.AdjustPaneSize({ 'Left', 3 }) },
             { key = 'j', mods = 'SHIFT', action = act.AdjustPaneSize({ 'Down', 3 }) },
             { key = 'k', mods = 'SHIFT', action = act.AdjustPaneSize({ 'Up', 3 }) },
             { key = 'l', mods = 'SHIFT', action = act.AdjustPaneSize({ 'Right', 3 }) },
 
-            -- Copy
+            -- copy
             { key = '[', action = act.ActivateCopyMode },
             { key = 'y', mods = 'CTRL', action = act.QuickSelect },
         },
     },
 
-    -- Mousebind
+    -- mousebind
     mouse_bindings = {
         {
             event = { Up = { streak = 1, button = 'Left' } },
