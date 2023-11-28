a6cc3324dd7055bbcf3c0785aa213db407db301d
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-28 21:34:35 +0900

    feat: move git plugins

--- config/common/HOME/.config/nvim/lua/plugins/default.lua
+++ config/common/HOME/.config/nvim/lua/plugins/default.lua
@@ -76,24 +76,4 @@ return {
             })
         end,
     },
-    -- -------------------------- Git  -------------------------- --
-    { -- git client
-        'NeogitOrg/neogit',
-        cond = not vim.g.vscode,
-        cmd = 'Neogit',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope.nvim',
-            { 'sindrets/diffview.nvim', cmd = 'DiffviewOpen' },
-        },
-        keys = {
-            { '<leader>ig', '<cmd>Neogit<cr>', desc = 'Neogit' },
-            { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'Diffview' },
-        },
-        config = true,
-    },
-    {
-        'kaarmu/typst.vim',
-        ft = 'typst',
-    },
 }
