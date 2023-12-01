47a0835e3970556a4ac72bdfbb3042d688c727a0
s3igo <85787242+s3igo@users.noreply.github.com>
2023-12-01 12:36:28 +0900

    chore: update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,10 +6,5 @@ eval "$(curl -fsSL raw.githubusercontent.com/s3igo/dotfiles/main/scripts/init.sh
 # clone dotfiles
 git clone https://github.com/s3igo/dotfiles.git ~/.dotfiles
 
-# scripts/link.sh
-eval "$(curl -fsSL raw.githubusercontent.com/s3igo/dotfiles/main/scripts/link.sh)"
-
-# restart shell
-exec $SHELL -l
-
-exit 0
+# link dotfiles
+source ~/.dotfiles/scripts/link.sh
