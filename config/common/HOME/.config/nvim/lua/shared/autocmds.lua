58e06905ee2bcb07cc5faf76f27560acb366299a
s3igo <85787242+s3igo@users.noreply.github.com>
2023-12-01 14:47:04 +0900

    chore: remove unused files

--- config/common/HOME/.config/nvim/lua/shared/autocmds.lua
+++
@@ -1,4 +0,0 @@
-vim.api.nvim_create_autocmd('FileType', {
-    desc = 'Set formatoptions',
-    command = 'setlocal formatoptions-=ro',
-})
--- config/common/HOME/.config/zsh/.zshrc
+++
@@ -1,2 +0,0 @@
-type sheldon > /dev/null 2>&1 && eval "$(sheldon source)"
-
