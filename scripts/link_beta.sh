18fe95689efd60be162f6e3cea49775c7042a9c4
s3igo <85787242+s3igo@users.noreply.github.com>
2023-12-01 12:48:08 +0900

    remove: shell hook

---
+++ scripts/link_beta.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+function inject-op {
+    declare FILENAME="$(basename "$FILE")"
+    declare TARGET="$(basename "${FILE%/*}")/${FILENAME}"
+
+    [[ "$TARGET" == 'git/config' ]] \
+        && echo -n 'op inject: ' \
+        && op inject -f -i "$FILE" -o "$DEST" \
+        && echo " -> ${FILE}" \
+        && return 0
+
+    return 1
+}
+
+# make file-based symlink
+echo '--- common config ---'
+declare LINK_DIR="${HOME}/.dotfiles/config/common/HOME"
+while read -r FILE; do
+    declare DEST="${HOME}${FILE##"$LINK_DIR"}"
+    mkdir -p "$(dirname "$DEST")"
+
+    inject-op && continue
+
+    ln -fnsv "$FILE" "$DEST"
+done < <(find "$LINK_DIR" -mindepth 1 -type f)
+
+# if macOS
+function karabiner {
+    [[ "$FILENAME" == karabiner.json ]] \
+        && echo -n 'cp: ' \
+        && cp -fv "$FILE" "$DEST" \
+        && return 0
+
+    return 1
+}
+
+if [[ "$(uname)" == 'Darwin' ]]; then
+    echo -e '\n--- MacOS specific config ---'
+    declare LINK_DIR="${HOME}/.dotfiles/config/mac/HOME"
+    while read -r FILE; do
+        declare DEST="${HOME}${FILE##"$LINK_DIR"}"
+        mkdir -p "$(dirname "$DEST")"
+
+        declare FILENAME="$(basename "$FILE")"
+
+        inject-op && continue
+        karabiner && continue
+
+        ln -fnsv "$FILE" "$DEST"
+    done < <(find "$LINK_DIR" -mindepth 1 -type f)
+    type goku > /dev/null 2>&1 && goku
+fi
