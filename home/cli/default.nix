768136007ef361bda5e053fef8fb6daa832b26f3
s3igo <85787242+s3igo@users.noreply.github.com>
2023-12-17 13:40:16 +0900

    refactor: split git config on nix

--- home/cli/default.nix
+++ home/cli/default.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }: {
-  imports = [ ./starship.nix ./zsh ];
+  imports = [
+    ./git.nix
+    ./starship.nix
+    ./zsh
+  ];
 
   programs = {
     bat.enable = true;
@@ -22,51 +26,8 @@
       enable = true;
       arguments = [ "--smart-case" ];
     };
-    git = {
-      enable = true;
-      aliases = {
-        co = "commit";
-        st = "status";
-        edit = "commit --amend";
-        append = "commit --amend --no-edit";
-        push-force = "push --force-with-lease --force-if-includes";
-        sync = "pull --rebase --autostash";
-        init-empty = "!git init && git commit --allow-empty -m 'initial commit'";
-        init-exist = "!git init && git add . && git commit -m 'first commit'";
-        create = ''!f() { gh repo create "$1" --private && ghq get -p "$1"; }; f'';
-        pr = ''!f() { git fetch origin "pull/$1/head:PR-$1"; }; f'';
-      };
-      delta = {
-        enable = true;
-        options = {
-          navigate = true;
-          side-by-side = true;
-        };
-      };
-      ignores = [
-        ".DS_Store"
-        ".env"
-        "node_modules/"
-      ];
-      lfs.enable = true;
-      userName = "s3igo";
-      userEmail = "85787242+s3igo@users.noreply.github.com";
-      signing = {
-        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINUyMJEMvBM/6QpZ365T7Gwf6KqYVuXKeTgDlKsFoU27";
-        signByDefault = true;
-      };
-      extraConfig = {
-        gpg = {
-          format = "ssh";
-          ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
-        };
-        ghq.root = "~/git";
-        core.ignorecase = "false";
-        push.default = "current";
-        pull.ff = "only";
-      };
-    };
   };
+
   home.packages = with pkgs; [
     fd
     ghq
