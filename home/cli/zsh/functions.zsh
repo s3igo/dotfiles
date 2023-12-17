0f436f2b112992994cb2c9d87f67e8d1743228e4
s3igo <85787242+s3igo@users.noreply.github.com>
2023-12-17 13:35:52 +0900

    feat: add git config on nix

--- home/cli/zsh/functions.zsh
+++ home/cli/zsh/functions.zsh
@@ -2,9 +2,9 @@ function timestamp {
     [[ $# == 0 ]] && date '+%Y%m%d%H%M%S' && return 0
     [[ $# > 1 ]] && echo 'error: too many arguments' && return 1
 
-    declare ext="''${1#*.}"
+    declare ext="${1#*.}"
     declare now="$(date +%Y%m%d%H%M%S)"
-    command mv "$1" "''${now}.''${ext}"
+    command mv "$1" "${now}.${ext}"
 }
 
 function rosetta {
@@ -16,9 +16,9 @@ function hashmv {
     [[ $# == 0 ]] && echo 'error: missing argument' && return 1
     [[ $# > 1 ]] && echo 'error: too many arguments' && return 1
 
-    declare EXT="''${1#*.}"
-    declare HASH="''$(shasum -a 256 "$1" | cut -d ' ' -f 1)"
-    declare NAME="''${HASH}.''${EXT}"
+    declare EXT="${1#*.}"
+    declare HASH="$(shasum -a 256 "$1" | cut -d ' ' -f 1)"
+    declare NAME="${HASH}.${EXT}"
 
     mv "$1" "$NAME"
     echo "$NAME"
--- home/cli/zsh/keybinds.zsh
+++ home/cli/zsh/keybinds.zsh
@@ -2,12 +2,12 @@ bindkey '^U' backward-kill-line
 
 function __ghq-fzf {
     declare ROOT="$(ghq root)"
-    declare PREVIEW_CMD="eza --tree --git-ignore -I 'node_modules|.git' ''${ROOT}/{}"
-    declare DEST="''${ROOT}/$(ghq list | fzf --preview ''${PREVIEW_CMD})"
-    declare BUFFER="cd ''${DEST}"
+    declare PREVIEW_CMD="eza --tree --git-ignore -I 'node_modules|.git' ${ROOT}/{}"
+    declare DEST="${ROOT}/$(ghq list | fzf --preview ${PREVIEW_CMD})"
+    declare BUFFER="cd ${DEST}"
     zle accept-line
-    mkdir -p "''${XDG_STATE_HOME}/ghq"
-    echo "$DEST" > "''${XDG_STATE_HOME}/ghq/lastdir"
+    mkdir -p "${XDG_STATE_HOME}/ghq"
+    echo "$DEST" > "${XDG_STATE_HOME}/ghq/lastdir"
 }
 zle -N __ghq-fzf
 bindkey '^G' __ghq-fzf
