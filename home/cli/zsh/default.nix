bafc61c9a7fb94cbfc5e568c0739c04899c83253
s3igo <85787242+s3igo@users.noreply.github.com>
2023-12-17 17:25:53 +0900

    feat: remove non-system pkgs

--- home/cli/zsh/default.nix
+++ home/cli/zsh/default.nix
@@ -72,10 +72,11 @@
       };
     };
     sessionVariables = {
-      _ZO_DATA_DIR = "${config.xdg.dataHome}/zoxide";
       DOCKER_CONFIG = "${config.xdg.configHome}/docker";
       EDITOR = "nvim";
+      LESSHISTFILE = "-";
       SSH_AUTH_SOCK = "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+      _ZO_DATA_DIR = "${config.xdg.dataHome}/zoxide";
     };
     initExtraFirst = ''eval "$(/opt/homebrew/bin/brew shellenv)"'';
     initExtra = ''
