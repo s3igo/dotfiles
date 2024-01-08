e4dcd7de8922afe06fa0f5716a8a5b3051643b67
s3igo <85787242+s3igo@users.noreply.github.com>
2024-01-08 17:57:20 +0900

    feat: split zsh config

--- home/cli/zsh/default.nix
+++ home/cli/zsh/default.nix
@@ -5,53 +5,24 @@
 }: {
   programs.zsh = {
     enable = true;
-    enableAutosuggestions = true;
-    enableCompletion = true;
-    defaultKeymap = "emacs";
     dotDir = ".config/zsh";
     history = {
       ignoreAllDups = true;
       ignorePatterns = ["cd *"];
       path = "${config.xdg.stateHome}/zsh/history";
     };
-    historySubstringSearch = {
-      enable = true;
-      searchUpKey = ["^P"];
-      searchDownKey = ["^N"];
-    };
     shellAliases = {
-      g = "git";
       lzd = "lazydocker";
       lg = "lazygit";
-      nv = "nvim";
-      restart = "exec $SHELL -l";
       trash = "trash -F";
-      # -iv: interactive, verbose
-      cp = "cp -iv";
-      mv = "mv -iv";
-      rm = "rm -iv";
-      # cd
-      ".." = "cd ..";
-      "..." = "cd ../..";
-      "...." = "cd ../../..";
-      "....." = "cd ../../../..";
-      "......" = "cd ../../../../..";
-      "......." = "cd ../../../../../..";
-      cdla = "cd $_";
       cdl = ''cd "$(cat ${config.xdg.dataHome}/lf/lastdir)"'';
       cdf = pkgs.lib.concatStrings [
         ''cd "$(${pkgs.fd}/bin/fd --hidden --no-ignore --type=directory --exclude=.git''
         ''| ${pkgs.fzf}/bin/fzf --preview "${pkgs.eza}/bin/eza -la --icons --git {}")"''
       ];
       cdg = ''cd "$(cat ${config.xdg.stateHome}/ghq/lastdir)"'';
-      # typos
-      al = "la";
-      mkdri = "mkdir";
     };
     shellGlobalAliases = {
-      "@i" = "install";
-      "@u" = "uninstall";
-      "@s" = "search";
       "@d_ps" = ''"$(docker ps | tail -n +2 | ${pkgs.fzf}/bin/fzf | awk '{print $1}')"'';
       "@d_ps-a" = ''"$(docker ps -a | tail -n +2 | ${pkgs.fzf}/bin/fzf | awk '{print $1}')"'';
       "@d_image_ls" = ''"$(docker image ls | tail -n +2 | ${pkgs.fzf}/bin/fzf | awk '{print $3}')"'';
@@ -60,24 +31,18 @@
       "@pst" = ''"$(pbpaste)"'';
       "@icloud" = "~/Library/Mobile\\ Documents/com~apple~CloudDocs";
     };
-    syntaxHighlighting = {
-      enable = true;
-      highlighters = ["main" "line" "brackets" "cursor"];
-      styles = {
-        alias = "fg=cyan,bold";
-        path = "fg=yellow,bold";
-        root = "bg=red";
-      };
-    };
     sessionVariables = {
       DOCKER_CONFIG = "${config.xdg.configHome}/docker";
       LESSHISTFILE = "-";
       SSH_AUTH_SOCK = "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
       _ZO_DATA_DIR = "${config.xdg.dataHome}/zoxide";
     };
-    initExtraFirst = ''eval "$(/opt/homebrew/bin/brew shellenv)"'';
+    initExtraFirst = ''
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+
+      ${import ../../../packages/zsh/config.nix {inherit pkgs;} }
+    '';
     initExtra = ''
-      source ${pkgs.zsh-autopair}/share/zsh/zsh-autopair/autopair.zsh
       source ${./options.zsh}
       source ${./functions.zsh}
 
