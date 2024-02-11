2c6c029170fb17ce4aaceebd4fb5f4bc97b44d07
s3igo <85787242+s3igo@users.noreply.github.com>
2024-02-11 20:05:02 +0900

    refactor(nix): nixvim config

--- packages/neovim/modules/plugins/coding.nix
+++ packages/neovim/modules/plugins/coding.nix
@@ -1,64 +1,104 @@
+{ pkgs, ... }:
 {
-  luasnip.enable = true;
-  friendly-snippets.enable = true;
-  lspkind = {
-    enable = true;
-    mode = "symbol";
-  };
-  cmp-nvim-lsp.enable = true;
-  cmp-buffer.enable = true;
-  cmp-path.enable = true;
-  cmp_luasnip.enable = true;
-  cmp-cmdline.enable = true;
-  nvim-cmp = {
-    enable = true;
-    snippet.expand = "luasnip";
-    mapping = {
-      "<c-j>" = ''
-        cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-          else
-            cmp.complete()
+  plugins = {
+    luasnip.enable = true;
+    friendly-snippets.enable = true;
+    lspkind = {
+      enable = true;
+      mode = "symbol";
+    };
+    cmp-nvim-lsp.enable = true;
+    cmp-buffer.enable = true;
+    cmp-path.enable = true;
+    cmp_luasnip.enable = true;
+    cmp-cmdline.enable = true;
+    nvim-cmp = {
+      enable = true;
+      snippet.expand = "luasnip";
+      mapping = {
+        "<c-j>" = ''
+          cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              cmp.complete()
+              end
+          end)
+        '';
+        "<c-k>" = ''
+          cmp.mapping(function(fallback)
+            if cmp.visible() and cmp.get_active_entry() then
+              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              fallback()
             end
-        end)
-      '';
-      "<c-k>" = ''
-        cmp.mapping(function(fallback)
-          if cmp.visible() and cmp.get_active_entry() then
-            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-          else
-            fallback()
-          end
-        end)
-      '';
-      "<c-d>" = "cmp.mapping.scroll_docs(4)";
-      "<c-u>" = "cmp.mapping.scroll_docs(-4)";
-      "<tab>" = "cmp.mapping.confirm()";
+          end)
+        '';
+        "<c-d>" = "cmp.mapping.scroll_docs(4)";
+        "<c-u>" = "cmp.mapping.scroll_docs(-4)";
+        "<tab>" = "cmp.mapping.confirm()";
+      };
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "luasnip"; }
+        { name = "buffer"; }
+        { name = "path"; }
+      ];
     };
-    sources = [
-      { name = "nvim_lsp"; }
-      { name = "luasnip"; }
-      { name = "buffer"; }
-      { name = "path"; }
-    ];
-  };
-  copilot-lua = {
-    enable = true;
-    filetypes = {
-      txt = false;
-      yaml = true;
-    };
-    extraOptions = {
-      suggestion = {
-        auto_trigger = true;
-        keymap = {
-          accept_word = "<c-y>";
-          accept_line = "<c-l>";
+    copilot-lua = {
+      enable = true;
+      filetypes = {
+        txt = false;
+        yaml = true;
+      };
+      extraOptions = {
+        suggestion = {
+          auto_trigger = true;
+          keymap = {
+            accept_word = "<c-y>";
+            accept_line = "<c-l>";
+          };
         };
       };
     };
+    nvim-autopairs.enable = true;
+    comment-nvim.enable = true;
   };
-  nvim-autopairs.enable = true;
-  comment-nvim.enable = true;
+
+  extraPlugins = with pkgs.vimPlugins; [
+    nvim-surround
+    substitute-nvim
+    text-case-nvim
+  ];
+
+  extraConfigLuaPost = ''
+    -- substiture.nvim
+    vim.keymap.set('n', 's', require('substitute').operator)
+    vim.keymap.set('n', 'ss', require('substitute').line)
+    vim.keymap.set('n', 'S', require('substitute').eol)
+    vim.keymap.set('x', 's', require('substitute').visual)
+
+    -- friendly-snippets
+    require('luasnip.loaders.from_vscode').lazy_load()
+
+    -- LuaSnip
+    local vscode_dir = vim.fs.find('.vscode', {
+      upward = true,
+      type = 'directory',
+      path = vim.fn.getcwd(),
+      stop = vim.env.HOME,
+    })[1]
+
+    if vscode_dir then
+      local snippets = vim.fs.find(function(name) return name:match('%.code%-snippets$') end, {
+          limit = 10,
+          type = 'file',
+          path = vscode_dir,
+      })
+      local loader = require('luasnip.loaders.from_vscode')
+      for _, snippet in pairs(snippets) do
+          loader.load_standalone({ path = snippet })
+      end
+    end
+  '';
 }
