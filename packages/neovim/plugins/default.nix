29bd57def19d470b2ffa6d8b9eb2ef3d84791d39
s3igo <85787242+s3igo@users.noreply.github.com>
2024-02-12 02:38:11 +0900

    feat(nix): add utils to nixvim config

--- packages/neovim/plugins/default.nix
+++ packages/neovim/plugins/default.nix
@@ -2,5 +2,6 @@ _: {
   imports = [
     ./coding.nix
     ./editor.nix
+    ./utils
   ];
 }
---
+++ packages/neovim/plugins/utils/im-select-nvim.nix
@@ -0,0 +1,13 @@
+{ pkgs }:
+with pkgs;
+
+vimUtils.buildVimPlugin {
+  pname = "im-select-nvim";
+  version = "2024-01-28";
+  src = fetchFromGitHub {
+    owner = "keaising";
+    repo = "im-select.nvim";
+    rev = "ca1aebb8f5c8a0342ae99a0fcc8ebc49b5f2201e";
+    hash = "sha256-tyVGbfRoshuuUWkFlQa6YvoJJ4HMLmG5p8Y0EsP1Zig=";
+  };
+}
