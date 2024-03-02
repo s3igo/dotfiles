2bdf78b6044117490bdc423acdcdf4d6b2ea6ba2
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-02 22:30:15 +0900

    feat(editor): add typos-lsp config

--- packages/neovim/config/plugins/lsp.nix
+++ packages/neovim/config/plugins/lsp.nix
@@ -1,58 +1,62 @@
 { pkgs, ... }:
 {
   plugins = {
-    lsp.keymaps = {
-      diagnostic = {
-        "<leader>." = {
-          action = "open_float";
-          desc = "Open diagnostic float";
+    lsp = {
+      enable = true;
+      servers.typos-lsp.enable = true;
+      keymaps = {
+        diagnostic = {
+          "<leader>." = {
+            action = "open_float";
+            desc = "Open diagnostic float";
+          };
+          "]d" = {
+            action = "goto_next";
+            desc = "Go to next diagnostic";
+          };
+          "[d" = {
+            action = "goto_prev";
+            desc = "Go to previous diagnostic";
+          };
         };
-        "]d" = {
-          action = "goto_next";
-          desc = "Go to next diagnostic";
-        };
-        "[d" = {
-          action = "goto_prev";
-          desc = "Go to previous diagnostic";
-        };
-      };
-      lspBuf = {
-        "<leader>r" = {
-          action = "rename";
-          desc = "Rename symbol";
-        };
-        "<leader>f" = {
-          action = "format";
-          desc = "Format buffer";
-        };
-        K = "hover";
-        gK = {
-          action = "signature_help";
-          desc = "Show signature help";
-        };
-        gr = {
-          action = "references";
-          desc = "Show references";
-        };
-        gd = {
-          action = "definition";
-          desc = "Go to definition";
-        };
-        gD = {
-          action = "declaration";
-          desc = "Go to declaration";
-        };
-        gI = {
-          action = "implementation";
-          desc = "Go to implementation";
-        };
-        gy = {
-          action = "type_definition";
-          desc = "Go to type definition";
-        };
-        gs = {
-          action = "workspace_symbol";
-          desc = "Search workspace symbols";
+        lspBuf = {
+          "<leader>r" = {
+            action = "rename";
+            desc = "Rename symbol";
+          };
+          "<leader>f" = {
+            action = "format";
+            desc = "Format buffer";
+          };
+          K = "hover";
+          gK = {
+            action = "signature_help";
+            desc = "Show signature help";
+          };
+          gr = {
+            action = "references";
+            desc = "Show references";
+          };
+          gd = {
+            action = "definition";
+            desc = "Go to definition";
+          };
+          gD = {
+            action = "declaration";
+            desc = "Go to declaration";
+          };
+          gI = {
+            action = "implementation";
+            desc = "Go to implementation";
+          };
+          gy = {
+            action = "type_definition";
+            desc = "Go to type definition";
+          };
+          gs = {
+            action = "workspace_symbol";
+            desc = "Search workspace symbols";
+          };
         };
       };
     };
