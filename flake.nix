b1e0686731062dc8e1cf83c4098cb948a66c809e
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-04 12:11:08 +0900

    feat(nix): add versions task

--- flake.nix
+++ flake.nix
@@ -52,29 +52,39 @@
       let
         pkgs = import nixpkgs { inherit system; };
         tasks =
+          with pkgs;
           let
-            deploy = pkgs.writeShellApplication {
+            deploy = writeShellApplication {
               name = "task_deploy";
               runtimeInputs = [ nix-darwin.packages.${system}.default ];
               text = ''
                 sudo -v && darwin-rebuild switch --flake ".#$(scutil --get LocalHostName)"
               '';
             };
-            update = pkgs.writeShellApplication {
+            update = writeShellApplication {
               name = "task_update";
               runtimeInputs = [ deploy ];
               text = ''
                 nix flake update --commit-lock-file && task_deploy
               '';
             };
-            gc = pkgs.writeShellScriptBin "task_gc" ''
+            gc = writeShellScriptBin "task_gc" ''
               nix store gc --max 5G
             '';
+            versions = writeShellApplication {
+              name = "task_versions";
+              runtimeInputs = [ gawk ];
+              text = ''
+                nix profile diff-closures --profile /nix/var/nix/profiles/system \
+                  | awk 'BEGIN { RS="" } { par=$0 } END { print par }'
+              '';
+            };
           in
           [
             deploy
             update
             gc
+            versions
           ];
       in
       {
