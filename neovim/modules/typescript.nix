184f7cc00461b58b71fab956dbe124728f7125ea
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-15 18:08:59 +0900

    fix: remove duplicate config

--- neovim/modules/typescript.nix
+++ neovim/modules/typescript.nix
@@ -12,13 +12,6 @@
       regex
     ];
     lsp.servers.tsserver.enable = true;
-    none-ls = {
-      enable = true;
-      sources.formatting.prettier = {
-        enable = true;
-        disableTsServerFormatter = true;
-      };
-    };
     ts-autotag.enable = true;
     ts-context-commentstring.enable = true;
   };
