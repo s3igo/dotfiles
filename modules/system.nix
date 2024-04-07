06c35b0edd05b92efaf9165d9af43a2ffa3d6b94
s3igo <85787242+s3igo@users.noreply.github.com>
2024-04-08 02:53:44 +0900

    feat: add nix substituter

--- modules/system.nix
+++ modules/system.nix
@@ -8,6 +8,14 @@
       experimental-features = "nix-command flakes";
       extra-platforms = "x86_64-darwin aarch64-darwin";
       use-xdg-base-directories = true;
+      substituters = [
+        "https://nix-community.cachix.org?priority=41"
+        "https://numtide.cachix.org?priority=42"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      ];
     };
     extraOptions = ''
       !include ${config.age.secrets.github-nix-token.path}
