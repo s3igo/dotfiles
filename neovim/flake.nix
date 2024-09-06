1c25040f1224476e3916afbbb12b1f1c79a9f615
s3igo <85787242+s3igo@users.noreply.github.com>
2024-09-06 19:43:28 +0900

    feat: remove deprecated outputs

--- neovim/flake.nix
+++ neovim/flake.nix
@@ -6,18 +6,5 @@
     {
       withModules = import ./. { inherit nixvim; };
       modules = import ./modules { inherit nixvim; };
-      # NOTE: `nixosModules` is deprecated
-      nixosModules = {
-        full = ./modules/full.nix;
-        im-select = ./modules/im-select.nix;
-        lua = ./modules/lua.nix;
-        nix = ./modules/nix.nix;
-        rust = ./modules/rust.nix;
-        typescript = ./modules/typescript.nix;
-        json = ./modules/json.nix;
-        markdown = ./modules/markdown.nix;
-        prettier = ./modules/prettier.nix;
-        yaml = ./modules/yaml.nix;
-      };
     };
 }
