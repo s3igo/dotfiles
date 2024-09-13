4ac3a5264df27f1278fc6d82c27ba61970fad68f
s3igo <85787242+s3igo@users.noreply.github.com>
2024-09-14 06:32:47 +0900

    refactor(nix): packages

--- packages/default.nix
+++ packages/default.nix
@@ -5,13 +5,32 @@
 }:
 
 let
-  neovim = import ./neovim.nix { inherit makeNixvim neovim-config; };
   skk-dict = pkgs.callPackage ./skk-dict.nix { };
   yaskkserv2 = pkgs.callPackage ./yaskkserv2.nix { };
   yaskkserv2-dict = pkgs.callPackage ./yaskkserv2-dict.nix { inherit skk-dict yaskkserv2; };
+
+  toPackage = name: {
+    inherit name;
+    value = makeNixvim {
+      imports = with neovim-config; [
+        nixosModules.default
+        nixosModules.${name}
+      ];
+    };
+  };
+  moduleNames = builtins.attrNames neovim-config.nixosModules;
+  neovimPackages = builtins.listToAttrs (map toPackage moduleNames);
 in
 
-neovim
-// {
+{
   inherit skk-dict yaskkserv2 yaskkserv2-dict;
+  neovim = makeNixvim {
+    imports = with neovim-config.nixosModules; [
+      default
+      nix
+      lua
+      markdown
+    ];
+  };
 }
+// neovimPackages
