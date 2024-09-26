84df297fcd88370f6aac3abca14df4130091f9eb
s3igo <85787242+s3igo@users.noreply.github.com>
2024-09-26 20:27:00 +0900

    feat: add launchd service for rclone

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,4 +1,8 @@
-{ user, ... }:
+{ config, user, ... }:
+
+let
+  home = "/Users/${user}";
+in
 
 {
   imports = [
@@ -8,6 +12,30 @@
     ./homebrew.nix
     ./home-manager.nix
     ./system.nix
+    (
+      { pkgs, ... }:
+      {
+        launchd.user.agents.MntR2Crypt = {
+          path = with pkgs; [
+            rclone
+            coreutils
+          ];
+          environment.RCLONE_CONFIG = config.age.secrets.rclone-config.path;
+          script = ''
+            mkdir -p ${home}/mnt/r2_crypt && rclone mount r2_crypt: ${home}/mnt/r2_crypt \
+              -o modules=iconv,from_code=UTF-8,to_code=UTF-8 \
+              --vfs-cache-mode full \
+              --vfs-cache-max-age 1d
+          '';
+          serviceConfig = {
+            KeepAlive = true;
+            RunAtLoad = true;
+            StandardOutPath = "${home}/.local/state/mnt/r2_crypt/out.log";
+            StandardErrorPath = "${home}/.local/state/mnt/r2_crypt/err.log";
+          };
+        };
+      }
+    )
     # (
     #   { pkgs, ... }:
     #   {
@@ -38,7 +66,7 @@
 
   users.users.${user} = {
     name = user;
-    home = "/Users/${user}";
+    inherit home;
     # shell = pkgs.zsh;
   };
 }
