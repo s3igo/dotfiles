c4d86e64e7da933be1b617b63e38cd984afc2ab4
s3igo <85787242+s3igo@users.noreply.github.com>
2024-11-29 00:45:39 +0900

    feat(neovim): add extra packages

--- modules/private/flake/neovim/packages.nix
+++ modules/private/flake/neovim/packages.nix
@@ -8,31 +8,63 @@
 
     let
       inherit (inputs'.nixvim.legacyPackages) makeNixvim;
-      toPackage = name: {
-        name = "neovim-${name}";
-        value = makeNixvim {
-          imports = with neovim-config; [
-            nixosModules.default
-            nixosModules.${name}
-          ];
-        };
-      };
-      moduleNames = builtins.attrNames neovim-config.nixosModules;
-      derivedPackages = builtins.listToAttrs (map toPackage moduleNames);
+      inherit (neovim-config) nixosModules;
+      inherit (builtins) attrNames attrValues removeAttrs;
+      neovimPackages =
+        let
+          toPackage = name: {
+            name = "neovim-${name}";
+            value = makeNixvim {
+              imports = [
+                nixosModules.default
+                nixosModules.${name}
+              ];
+            };
+          };
+          moduleNames = attrNames (removeAttrs nixosModules [ "default" ]);
+        in
+        builtins.listToAttrs (map toPackage moduleNames);
+      neovimExtraPackages =
+        let
+          toPackageWithExtra = name: {
+            name = "neovim-extra-${name}";
+            value = makeNixvim {
+              imports = [
+                nixosModules.default
+                nixosModules.extra
+                nixosModules.${name}
+              ];
+            };
+          };
+          moduleNames = attrNames (
+            removeAttrs nixosModules [
+              "default"
+              "extra"
+            ]
+          );
+        in
+        builtins.listToAttrs (map toPackageWithExtra moduleNames);
     in
 
     {
-      packages = derivedPackages // {
-        neovim-full = makeNixvim { imports = builtins.attrValues neovim-config.nixosModules; };
-        neovim = makeNixvim {
-          imports = with neovim-config.nixosModules; [
-            default
-            extra
-            nix
-            lua
-            markdown
-          ];
+      packages =
+        neovimPackages
+        // neovimExtraPackages
+        // {
+          neovim-default = makeNixvim { imports = [ nixosModules.default ]; };
+          neovim-full = makeNixvim {
+            imports = attrValues (removeAttrs nixosModules [ "extra" ]);
+          };
+          neovim-extra-full = makeNixvim { imports = attrValues nixosModules; };
+          neovim = makeNixvim {
+            imports = with nixosModules; [
+              default
+              extra
+              nix
+              lua
+              markdown
+            ];
+          };
         };
-      };
     };
 }
