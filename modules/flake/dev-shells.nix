49ee5ced72426fbc98416474a6f717ec801ca720
s3igo <85787242+s3igo@users.noreply.github.com>
2025-03-23 23:50:48 +0900

    chore: Add nil and nixd to devShell

--- modules/flake/dev-shells.nix
+++ modules/flake/dev-shells.nix
@@ -11,6 +11,8 @@
       devShells.default = pkgs.mkShellNoCC {
         inputsFrom = [ config.mission-control.devShell ];
         packages = [
+          pkgs.nixd
+          pkgs.nil
           (neovim-config.lib.customName {
             inherit pkgs;
             nvim = config.packages.neovim;
