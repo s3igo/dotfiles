0dafa331e8bf2334f51392dfbe48402ca86c76c8
s3igo <85787242+s3igo@users.noreply.github.com>
2025-03-26 04:46:25 +0900

    chore(home): Remove obsolete options from Aider
    Ref: https://github.com/NixOS/nixpkgs /pull/385972

--- home/aider.nix
+++ home/aider.nix
@@ -9,27 +9,12 @@ let
   package =
     let
       aider-chat = pkgs.aider-chat.withPlaywright;
-      playwright =
-        let
-          pred = drv: drv.pname == "playwright";
-          drv = lib.findSingle pred "none" "multiple" aider-chat.dependencies;
-        in
-        assert drv != "none" && drv != "multiple";
-        drv;
     in
-    pkgs.runCommand "${aider-chat.name}-wrapped"
-      {
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-      }
-      # Ref: https://nixos.wiki/wiki/Playwright
-      # PLAYWRIGHT_BROWSERS_PATH: Related to https://github.com/Aider-AI/aider/issues/2192
-      # PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS: https://wiki.nixos.org/wiki/Playwright
+    pkgs.runCommand "${aider-chat.name}-wrapped" { nativeBuildInputs = [ pkgs.makeWrapper ]; }
       # --add-flags: Used instead of `--set` to specify API key at runtime
       ''
         mkdir -p $out/bin
         makeWrapper ${lib.getExe aider-chat} $out/bin/${aider-chat.meta.mainProgram} \
-          --set PLAYWRIGHT_BROWSERS_PATH "${playwright.driver.browsers}" \
-          --set PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS true \
           --add-flags '--anthropic-api-key $(cat ${osConfig.age.secrets.aider-anthropic.path})'
       '';
   yamlFormat = pkgs.formats.yaml { };
