8c3c0aeaffb55851931d853f5e90e336aefe6e80
s3igo <85787242+s3igo@users.noreply.github.com>
2025-03-26 00:40:29 +0900

    feat(home): Add default key bindings to fzf

--- home/fzf.nix
+++ home/fzf.nix
@@ -4,6 +4,16 @@
     changeDirWidgetCommand = "fd --type directory --hidden --follow --exclude .git";
     changeDirWidgetOptions = [ "--preview 'ls -la --color=always {}'" ];
     defaultCommand = "fd --type file --hidden --follow --exclude .git";
+    defaultOptions = [
+      "--bind=${
+        builtins.concatStringsSep "," [
+          "ctrl-u:preview-half-page-up"
+          "ctrl-d:preview-half-page-down"
+          "ctrl-j:accept"
+          "ctrl-k:kill-line"
+        ]
+      }"
+    ];
     # Set empty string to disable default key binding of <C-t>
     fileWidgetCommand = "";
   };
