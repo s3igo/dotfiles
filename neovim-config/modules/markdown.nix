566dbd6152821277879cb942a80fa438e0d43dc3
s3igo <85787242+s3igo@users.noreply.github.com>
2025-04-16 07:21:51 +0900

    chore(neovim): Comment out temporarily
    Markdown-specific autoCmds seem unnecessary

--- neovim-config/modules/markdown.nix
+++ neovim-config/modules/markdown.nix
@@ -1,16 +1,16 @@
 {
-  autoCmd = [
-    {
-      event = "FileType";
-      pattern = "markdown";
-      callback.__raw = ''
-        function()
-          vim.opt_local.formatoptions:append('r')
-          vim.opt_local.comments = 'b:-,n:>'
-        end
-      '';
-    }
-  ];
+  # autoCmd = [
+  #   {
+  #     event = "FileType";
+  #     pattern = "markdown";
+  #     callback.__raw = ''
+  #       function()
+  #         vim.opt_local.formatoptions:append('r')
+  #         vim.opt_local.comments = 'b:-,n:>'
+  #       end
+  #     '';
+  #   }
+  # ];
 
   highlight = {
     "@markup.list.checked.markdown".link = "NightflyTurquoiseMode";
