2e1faac9e95a4d01c27dfea6625916e0aaf84d9b
s3igo <85787242+s3igo@users.noreply.github.com>
2025-04-20 06:37:08 +0900

    feat(system): trusted-users設定を追加
    `sandbox = true`で実行できるようにするため

--- modules/system.nix
+++ modules/system.nix
@@ -19,6 +19,10 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       ];
+      trusted-users = [
+        "root"
+        "s3igo"
+      ];
     };
     optimise.automatic = true;
     extraOptions = ''
