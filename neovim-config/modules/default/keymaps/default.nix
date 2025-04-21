4d7ebb7a5967ffe2a595dd5a4d32b9e6aebbc166
s3igo <85787242+s3igo@users.noreply.github.com>
2025-04-21 10:27:58 +0900

    refactor(neovim): Remove unused keymaps

--- neovim-config/modules/default/keymaps/default.nix
+++ neovim-config/modules/default/keymaps/default.nix
@@ -16,10 +16,10 @@ utils@{ mapMode, ... }:
       }
     ]
     ++ map (mapMode "n") [
-      {
-        key = "<c-[>";
-        action = "<cmd>noh<cr>";
-      }
+      # {
+      #   key = "<c-[>";
+      #   action = "<cmd>noh<cr>";
+      # }
       {
         key = "<leader>c";
         action.__raw = ''
@@ -66,74 +66,16 @@ utils@{ mapMode, ... }:
         '';
         options.desc = "Toggle trailing comma";
       }
-      # files
-      # {
-      #   key = "<leader>s";
-      #   action = "<cmd>w<cr>";
-      #   options.desc = "Save";
-      # }
-      # {
-      #   key = "<leader>q";
-      #   action = "<cmd>qa<cr>";
-      #   options.desc = "Quit all";
-      # }
-      # ]
-      # ++
-      #   map
-      #     (mapMode [
-      #       "n"
-      #       "x"
-      #     ])
-      #     [
-      #       # registers
-      #       {
-      #         key = "<leader>y";
-      #         action = ''"+y'';
-      #         options.desc = "Yank to system clipboard";
-      #       }
-      #       {
-      #         key = "<leader>Y";
-      #         action = ''"+Y'';
-      #         options.desc = "Yank to system clipboard (until end of line)";
-      #       }
-      #       {
-      #         key = "<leader>p";
-      #         action = ''"+p'';
-      #         options.desc = "Paste from system clipboard";
-      #       }
-      #       {
-      #         key = "<leader>P";
-      #         action = ''"+P'';
-      #         options.desc = "Paste from system clipboard (before)";
-      #       }
-      #     ]
-      # ++
-      #   map
-      #     (mapMode [
-      #       "n"
-      #       "x"
-      #       "o"
-      #     ])
-      #     [
-      #       # helix keybindings
-      #       {
-      #         key = "gl";
-      #         action = "g_";
-      #       }
-      #       {
-      #         key = "gh";
-      #         action = "g^";
-      #       }
-      #     ]
-      # ++ map (mapMode "i") [
-      #   # indent
-      #   {
-      #     key = "<tab>";
-      #     action = "<c-t>";
-      #   }
-      #   {
-      #     key = "<s-tab>";
-      #     action = "<c-d>";
-      #   }
+    ]
+    ++ map (mapMode "i") [
+      # indent
+      {
+        key = "<tab>";
+        action = "<c-t>";
+      }
+      {
+        key = "<s-tab>";
+        action = "<c-d>";
+      }
     ];
 }
--- neovim-config/modules/default/keymaps/emacs.nix
+++ neovim-config/modules/default/keymaps/emacs.nix
@@ -3,29 +3,29 @@
 {
   keymaps =
     map (mapMode "i") [
-      # {
-      #   key = "<c-t>";
-      #   action.__raw = ''
-      #     function()
-      #         local line = vim.api.nvim_get_current_line()
-      #         local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-      #         -- If cursor is at the beginning of line or line has only 1 char
-      #         if col == 0 or #line == 1 then
-      #           return
-      #         end
-      #         -- If cursor is at the end of line
-      #         if col == #line then
-      #           col = col - 1
-      #           vim.api.nvim_win_set_cursor(0, { row, col })
-      #         end
-      #         local lhs_char = line:sub(col, col)
-      #         local rhs_char = line:sub(col + 1, col + 1)
-      #         vim.api.nvim_buf_set_text(0, row - 1, col - 1, row - 1, col + 1, { rhs_char .. lhs_char })
-      #         vim.api.nvim_win_set_cursor(0, { row, col + 1 })
-      #     end
-      #   '';
-      #   options.desc = "transpose";
-      # }
+      {
+        key = "<c-t>";
+        action.__raw = ''
+          function()
+              local line = vim.api.nvim_get_current_line()
+              local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+              -- If cursor is at the beginning of line or line has only 1 char
+              if col == 0 or #line == 1 then
+                return
+              end
+              -- If cursor is at the end of line
+              if col == #line then
+                col = col - 1
+                vim.api.nvim_win_set_cursor(0, { row, col })
+              end
+              local lhs_char = line:sub(col, col)
+              local rhs_char = line:sub(col + 1, col + 1)
+              vim.api.nvim_buf_set_text(0, row - 1, col - 1, row - 1, col + 1, { rhs_char .. lhs_char })
+              vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+          end
+        '';
+        options.desc = "transpose chars";
+      }
       {
         key = "<c-k>";
         action.__raw = ''
@@ -73,10 +73,10 @@
         key = "<a-b>";
         action = "<c-g>U<s-left>";
       }
-      # {
-      #   key = "<c-d>";
-      #   action = "<del>";
-      # }
+      {
+        key = "<c-d>";
+        action = "<del>";
+      }
     ]
     ++ map (mapMode "c") [
       {
