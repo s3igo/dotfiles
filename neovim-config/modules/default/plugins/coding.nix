352e8ce3892f2c82938920f747ee84613332ba8b
s3igo <85787242+s3igo@users.noreply.github.com>
2025-04-22 13:22:30 +0900

    feat(neovim): Organize coding plugins
    - Replace nvim-surround with mini.surround
    - Replace autoclose.nvim with mini.pairs

--- neovim-config/modules/default/plugins/coding.nix
+++ neovim-config/modules/default/plugins/coding.nix
@@ -99,7 +99,30 @@ _:
         };
       };
     };
-    nvim-surround.enable = true;
-    autoclose.enable = true;
+    mini = {
+      enable = true;
+      luaConfig = {
+        pre = "local ai_spec = require('mini.extra').gen_ai_spec";
+        # Not using NixVim's built-in keymap to avoid complex handling of escape sequences
+        post = ''
+          local map_bs = function(lhs, rhs)
+            vim.keymap.set('i', lhs, rhs, { expr = true, replace_keycodes = false })
+          end
+          map_bs('<C-h>', 'v:lua.MiniPairs.bs()')
+          map_bs('<C-w>', 'v:lua.MiniPairs.bs("\23")')
+          map_bs('<C-u>', 'v:lua.MiniPairs.bs("\21")')
+        '';
+      };
+      modules = {
+        pairs = { };
+        surround = { };
+        ai.custom_textobjects = {
+          I.__raw = "ai_spec.indent()";
+          L.__raw = "ai_spec.line()";
+          N.__raw = "ai_spec.number()";
+        };
+        extra = { };
+      };
+    };
   };
 }
--- neovim-config/modules/default/plugins/treesitter.nix
+++ neovim-config/modules/default/plugins/treesitter.nix
@@ -6,134 +6,134 @@ _: {
       settings = {
         highlight.enable = true;
         indent.enable = true;
-        incrementalSelection = {
-          enable = true;
-          keymaps = {
-            initSelection = "<tab>";
-            scopeIncremental = "<cr>";
-            nodeIncremental = "<tab>";
-            nodeDecremental = "<s-tab>";
-          };
-        };
+        # incrementalSelection = {
+        #   enable = true;
+        #   keymaps = {
+        #     initSelection = "<tab>";
+        #     scopeIncremental = "<cr>";
+        #     nodeIncremental = "<tab>";
+        #     nodeDecremental = "<s-tab>";
+        #   };
+        # };
       };
     };
-    treesitter-textobjects = {
-      enable = true;
-      select = {
-        enable = true;
-        lookahead = true;
-        keymaps = {
-          ak = {
-            query = "@block.outer";
-            desc = "Around block";
-          };
-          ik = {
-            query = "@block.inner";
-            desc = "Inside block";
-          };
-          ac = {
-            query = "@class.outer";
-            desc = "Around class";
-          };
-          ic = {
-            query = "@class.inner";
-            desc = "Inside class";
-          };
-          "a?" = {
-            query = "@conditional.outer";
-            desc = "Around conditional";
-          };
-          "i?" = {
-            query = "@conditional.inner";
-            desc = "Inside conditional";
-          };
-          af = {
-            query = "@function.outer";
-            desc = "Around function";
-          };
-          "if" = {
-            query = "@function.inner";
-            desc = "Inside function";
-          };
-          al = {
-            query = "@loop.outer";
-            desc = "Around loop";
-          };
-          il = {
-            query = "@loop.inner";
-            desc = "Inside loop";
-          };
-          aa = {
-            query = "@parameter.outer";
-            desc = "Around argument";
-          };
-          ia = {
-            query = "@parameter.inner";
-            desc = "Inside argument";
-          };
-        };
-      };
-      move = {
-        enable = true;
-        setJumps = true;
-        gotoNextStart = {
-          "]k" = {
-            query = "@block.outer";
-            desc = "Next block start";
-          };
-          "]f" = {
-            query = "@function.outer";
-            desc = "Next function start";
-          };
-          "]a" = {
-            query = "@parameter.inner";
-            desc = "Next argument start";
-          };
-        };
-        gotoNextEnd = {
-          "]K" = {
-            query = "@block.outer";
-            desc = "Next block end";
-          };
-          "]F" = {
-            query = "@function.outer";
-            desc = "Next function end";
-          };
-          "]A" = {
-            query = "@parameter.inner";
-            desc = "Next argument end";
-          };
-        };
-        gotoPreviousStart = {
-          "[k" = {
-            query = "@block.outer";
-            desc = "Previous block start";
-          };
-          "[f" = {
-            query = "@function.outer";
-            desc = "Previous function start";
-          };
-          "[a" = {
-            query = "@parameter.inner";
-            desc = "Previous argument start";
-          };
-        };
-        gotoPreviousEnd = {
-          "[K" = {
-            query = "@block.outer";
-            desc = "Previous block end";
-          };
-          "[F" = {
-            query = "@function.outer";
-            desc = "Previous function end";
-          };
-          "[A" = {
-            query = "@parameter.inner";
-            desc = "Previous argument end";
-          };
-        };
-      };
-    };
+    # treesitter-textobjects = {
+    #   enable = true;
+    #   select = {
+    #     enable = true;
+    #     lookahead = true;
+    #     keymaps = {
+    #       ak = {
+    #         query = "@block.outer";
+    #         desc = "Around block";
+    #       };
+    #       ik = {
+    #         query = "@block.inner";
+    #         desc = "Inside block";
+    #       };
+    #       ac = {
+    #         query = "@class.outer";
+    #         desc = "Around class";
+    #       };
+    #       ic = {
+    #         query = "@class.inner";
+    #         desc = "Inside class";
+    #       };
+    #       "a?" = {
+    #         query = "@conditional.outer";
+    #         desc = "Around conditional";
+    #       };
+    #       "i?" = {
+    #         query = "@conditional.inner";
+    #         desc = "Inside conditional";
+    #       };
+    #       af = {
+    #         query = "@function.outer";
+    #         desc = "Around function";
+    #       };
+    #       "if" = {
+    #         query = "@function.inner";
+    #         desc = "Inside function";
+    #       };
+    #       al = {
+    #         query = "@loop.outer";
+    #         desc = "Around loop";
+    #       };
+    #       il = {
+    #         query = "@loop.inner";
+    #         desc = "Inside loop";
+    #       };
+    #       aa = {
+    #         query = "@parameter.outer";
+    #         desc = "Around argument";
+    #       };
+    #       ia = {
+    #         query = "@parameter.inner";
+    #         desc = "Inside argument";
+    #       };
+    #     };
+    #   };
+    #   move = {
+    #     enable = true;
+    #     setJumps = true;
+    #     gotoNextStart = {
+    #       "]k" = {
+    #         query = "@block.outer";
+    #         desc = "Next block start";
+    #       };
+    #       "]f" = {
+    #         query = "@function.outer";
+    #         desc = "Next function start";
+    #       };
+    #       "]a" = {
+    #         query = "@parameter.inner";
+    #         desc = "Next argument start";
+    #       };
+    #     };
+    #     gotoNextEnd = {
+    #       "]K" = {
+    #         query = "@block.outer";
+    #         desc = "Next block end";
+    #       };
+    #       "]F" = {
+    #         query = "@function.outer";
+    #         desc = "Next function end";
+    #       };
+    #       "]A" = {
+    #         query = "@parameter.inner";
+    #         desc = "Next argument end";
+    #       };
+    #     };
+    #     gotoPreviousStart = {
+    #       "[k" = {
+    #         query = "@block.outer";
+    #         desc = "Previous block start";
+    #       };
+    #       "[f" = {
+    #         query = "@function.outer";
+    #         desc = "Previous function start";
+    #       };
+    #       "[a" = {
+    #         query = "@parameter.inner";
+    #         desc = "Previous argument start";
+    #       };
+    #     };
+    #     gotoPreviousEnd = {
+    #       "[K" = {
+    #         query = "@block.outer";
+    #         desc = "Previous block end";
+    #       };
+    #       "[F" = {
+    #         query = "@function.outer";
+    #         desc = "Previous function end";
+    #       };
+    #       "[A" = {
+    #         query = "@parameter.inner";
+    #         desc = "Previous argument end";
+    #       };
+    #     };
+    #   };
+    # };
     treesitter-context.enable = true;
     rainbow-delimiters.enable = true;
   };
