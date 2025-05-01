72b51dc159c62ac00dba0633506ebef1eba6aba3
s3igo <85787242+s3igo@users.noreply.github.com>
2025-05-01 19:48:35 +0900

    fix(neovim): Fix deno-script module configuration
    Follow changes in Neovim v0.11

--- neovim-config/modules/deno-script.nix
+++ neovim-config/modules/deno-script.nix
@@ -1,5 +1,13 @@
 {
   imports = [ ./deno.nix ];
 
-  plugins.lsp.servers.denols.rootDir = "function() return '' end";
+  plugins.lsp.servers.denols.rootMarkers = [
+    {
+      __raw = ''
+        vim.fs.root(0, function(name, path)
+          return name:match('%.[jt]sx?$') ~= nil
+        end)
+      '';
+    }
+  ];
 }
