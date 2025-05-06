6388781f0da92991a591e04c8fe574b313afc258
s3igo <85787242+s3igo@users.noreply.github.com>
2025-05-07 01:16:26 +0900

    feat(neovim): Replace im-select with custom impl

--- neovim-config/modules/default/input-method.nix
+++ neovim-config/modules/default/input-method.nix
@@ -8,11 +8,14 @@ _:
   );
 
   extraPackages = lib.optional pkgs.stdenv.isDarwin (
-    pkgs.callPackage ../../../packages/macism.nix { }
+    pkgs.callPackage ../../../packages/im-select-swift.nix { }
   );
 
   extraConfigLua = lib.optionalString pkgs.stdenv.isDarwin ''
     -- im-select
-    require('im_select').setup({ default_im_select = "net.mtgto.inputmethod.macSKK.ascii" })
+    require('im_select').setup({
+      default_im_select = "net.mtgto.inputmethod.macSKK.ascii",
+      default_command = "im-select-swift"
+    })
   '';
 }
