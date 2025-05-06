bc69e97d3cfcc932d1e33bbd3c348202422d2f8d
s3igo <85787242+s3igo@users.noreply.github.com>
2025-05-06 19:43:49 +0900

    feat(pkg): Add custom implementation of im-select

---
+++ packages/im-select-swift.nix
@@ -0,0 +1,79 @@
+{ lib, swift }:
+
+swift.stdenv.mkDerivation rec {
+  pname = "im-select-swift";
+  version = "0.1.0";
+
+  dontUnpack = true;
+  dontConfigure = true;
+
+  src = builtins.toFile "main.swift" ''
+    import InputMethodKit
+
+    func getCurrentInputSourceID() -> String? {
+      guard
+        let currentSource = TISCopyCurrentKeyboardInputSource()?
+          .takeRetainedValue(),
+        let propertyPtr = TISGetInputSourceProperty(
+          currentSource, kTISPropertyInputSourceID)
+      else { return nil }
+      return Unmanaged<CFString>.fromOpaque(propertyPtr).takeUnretainedValue()
+        as String
+    }
+
+    func switchToInputSource(id: String) -> Bool {
+      let filter = [kTISPropertyInputSourceID: id] as CFDictionary
+      guard
+        let sourceList = TISCreateInputSourceList(filter, false)?
+          .takeRetainedValue(), CFArrayGetCount(sourceList) > 0
+      else { return false }
+      let inputSource = unsafeBitCast(
+        CFArrayGetValueAtIndex(sourceList, 0), to: TISInputSource.self)
+      return TISSelectInputSource(inputSource) == noErr
+    }
+
+    func main() {
+      guard let currentID = getCurrentInputSourceID() else { exit(1) }
+
+      if CommandLine.arguments.count <= 1 {
+        print(currentID)
+        exit(0)
+      }
+
+      let targetID = CommandLine.arguments[1]
+
+      if currentID == targetID {
+        print(targetID)
+        exit(0)
+      }
+
+      if switchToInputSource(id: targetID) { print(targetID) } else { exit(1) }
+    }
+
+    main()
+  '';
+
+  nativeBuildInputs = [ swift ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    swiftc -O -Xlinker -dead_strip $src -o ${pname}
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp ${pname} $out/bin
+
+    runHook postInstall
+  '';
+
+  meta = {
+    mainProgram = "im-select-swift";
+    platforms = lib.platforms.darwin;
+  };
+}
