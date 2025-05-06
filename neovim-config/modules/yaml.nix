1442188973021accc5423b34d7cf377d40fe9739
s3igo <85787242+s3igo@users.noreply.github.com>
2025-05-07 00:38:22 +0900

    feat(neovim): Add actionlint to yaml module

--- neovim-config/modules/yaml.nix
+++ neovim-config/modules/yaml.nix
@@ -1,8 +1,14 @@
 {
-  plugins.lsp.servers.yamlls = {
-    enable = true;
-    settings.schemas = {
-      "https://json.schemastore.org/github-workflow.json" = ".github/workflows/*.{yml,yaml}";
+  plugins = {
+    lsp.servers.yamlls = {
+      enable = true;
+      settings.schemas = {
+        "https://json.schemastore.org/github-workflow.json" = ".github/workflows/*.{yml,yaml}";
+      };
+    };
+    none-ls = {
+      enable = true;
+      sources.diagnostics.actionlint.enable = true;
     };
   };
 }
