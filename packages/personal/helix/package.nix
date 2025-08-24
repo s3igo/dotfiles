1cac6b36811c9a63dd77a58e962f5dfaf4f1d373
s3igo <85787242+s3igo@users.noreply.github.com>
2025-08-24 19:54:43 +0900

    feat(pkg): helixの完全な設定を追加

--- packages/personal/helix/package.nix
+++ packages/personal/helix/package.nix
@@ -1,27 +1,37 @@
 {
   lib,
   formats,
-  helix,
   runCommand,
   makeBinaryWrapper,
-}:
+  helix,
 
-let
-  settings = {
+  nixd,
+  nil,
+  taplo,
+  typos-lsp,
+
+  settings ? {
     editor.rulers = [
       80
       100
       120
     ];
-  };
+  },
+}:
 
+let
   tomlFormat = formats.toml { };
   configFile = tomlFormat.generate "config.toml" settings;
 in
 
-runCommand "helix-wrapped" { nativeBuildInputs = [ makeBinaryWrapper ]; } ''
-  mkdir -p $out/bin
-  makeBinaryWrapper ${lib.getExe helix} $out/bin/${helix.meta.mainProgram} \
-    --inherit-argv0 \
-    --add-flags '--config ${configFile}'
-''
+runCommand "helix-wrapped"
+  {
+    nativeBuildInputs = [ makeBinaryWrapper ];
+    meta.mainProgram = helix.meta.mainProgram;
+  }
+  ''
+    mkdir -p $out/bin
+    makeBinaryWrapper ${lib.getExe helix} $out/bin/${helix.meta.mainProgram} \
+      --inherit-argv0 \
+      --add-flags '--config ${configFile}'
+  ''
