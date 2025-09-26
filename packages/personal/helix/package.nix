0c06f8c30d6be3a5c6d1046da3d9cc0fa97bb503
s3igo <85787242+s3igo@users.noreply.github.com>
2025-09-26 18:32:31 +0900

    feat(pkg): Helixの設定にthemesとignoresを追加

--- packages/personal/helix/package.nix
+++ packages/personal/helix/package.nix
@@ -1,6 +1,6 @@
 {
   lib,
-  runCommand,
+  runCommandNoCC,
   makeBinaryWrapper,
 
   helix,
@@ -12,14 +12,14 @@
   typos-lsp,
   vtsls,
 
-  settings ? {
+  helixSettings ? {
     editor.rulers = [
       80
       100
       120
     ];
   },
-  languages ? {
+  helixLanguages ? {
     language-server = {
       vtsls = {
         command = lib.getExe vtsls;
@@ -33,21 +33,81 @@
       }
     ];
   },
+  helixThemes ?
+    let
+      mkTransparentTheme = name: {
+        inherits = name;
+        "ui.background" = "none";
+      };
+      themes = [
+        "jetbrains_dark"
+        "catppuccin_mocha"
+        "iceberg-dark"
+      ];
+      transparentThemes = builtins.listToAttrs (
+        map (theme: {
+          name = "${theme}_transparent";
+          value = mkTransparentTheme theme;
+        }) themes
+      );
+    in
+    transparentThemes
+    // {
+      iceberg-custom = transparentThemes.iceberg-dark_transparent // {
+        "ui.gutter" = {
+          fg = "linenr_fg";
+          bg = "none";
+        };
+        "ui.linenr" = {
+          bg = "none";
+        };
+        "ui.linenr.selected" = {
+          fg = "linenr_fg";
+          bg = "none";
+        };
+      };
+      catppuccin-custom = transparentThemes.catppuccin_mocha_transparent // {
+        "ui.linenr" = "overlay2";
+      };
+    },
+  helixIgnores ? [
+    ".direnv/"
+    "result"
+    "!.helix/"
+    "!.github/"
+    "!.gitignore"
+    "!.gitattributes"
+    "!*.local.*"
+    "!*.local/"
+  ],
 }:
 
 let
   tomlFormat = formats.toml { };
-  configFile = tomlFormat.generate "config.toml" settings;
 in
 
-runCommand "helix-wrapped"
+runCommandNoCC "helix-personal"
   {
     nativeBuildInputs = [ makeBinaryWrapper ];
-    meta.mainProgram = helix.meta.mainProgram;
+    meta = helix.meta;
   }
   ''
+    mkdir -p $out/share/config/helix
+    cp ${tomlFormat.generate "config.toml" helixSettings} $out/share/config/helix/config.toml
+    cp ${tomlFormat.generate "languages.toml" helixLanguages} $out/share/config/helix/languages.toml
+    echo -n '${lib.concatLines helixIgnores}' > $out/share/config/helix/ignore
+
+    mkdir -p $out/share/config/helix/themes
+    ${lib.pipe helixThemes [
+      (lib.mapAttrsToList (
+        name: value:
+        "cp ${tomlFormat.generate "${name}.toml" value} $out/share/config/helix/themes/${name}.toml"
+      ))
+      (lib.concatLines)
+    ]}
+
     mkdir -p $out/bin
     makeBinaryWrapper ${lib.getExe helix} $out/bin/${helix.meta.mainProgram} \
       --inherit-argv0 \
-      --add-flags '--config ${configFile}'
+      --set XDG_CONFIG_HOME "$out/share/config"
   ''
