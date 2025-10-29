53f1a01e0683d1380fa7796602cd2e4959cb6540
s3igo <85787242+s3igo@users.noreply.github.com>
2025-10-30 05:40:56 +0900

    caesium-clt: 1.0.2 -> 1.1.0
    Diff: https://github.com/Lymphatus/caesium-clt/compare/v1.0.2...v1.1.0
    
    Changelog: https://github.com/Lymphatus/caesium-clt/blob/v1.1.0/CHANGELOG.md

--- packages/caesium-clt/package.nix
+++ packages/caesium-clt/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "caesium-clt";
-  version = "1.0.2";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "Lymphatus";
     repo = "caesium-clt";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-La+0KuZ7kLpivDR1CNb6yTtQqxgAdCI4osBt3N35LGk=";
+    hash = "sha256-WHEihEKULw2S20Pcz2n67sPhNn7cRB/YkF7LzQf/YKE=";
   };
 
-  cargoHash = "sha256-xWshZZO/7N89nfu9YDeBTGgd98Jtlw31HSQRlfBCsFY=";
+  cargoHash = "sha256-BYmvEokySY0L+ybeKpbA3rw7Wjt7/7ZRD2TKY1a6i1c=";
 
   meta = {
     description = "Caesium Command Line Tools - Lossy/lossless image compression tool";
